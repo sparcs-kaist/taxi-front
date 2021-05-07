c3e98ffe5c5cbba99d9d1b36ed0a8459f3dbe692
14KGun <geon6757@kaist.ac.kr>
2021-05-07 23:11:20 +0900

    edit Search and Setting page

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+    constructor(props){
+        super(props);
+
+        this.styleBox = { background: 'white' }
+        this.styleTitle = { fontSize: '22px', fontWeight: 900 }
+        this.styleStation = { position: 'absolute' }
+        this.styleStationL = { left: '0px', right: 'calc(50% + 10px)' }
+        this.styleStationR = { left: 'calc(50% + 10px)', right: '0px' }
+        this.styleSearchBtn = {
+            height: '40px', borderRadius: '20px',
+            background: 'rgb(127,89,202)', color: 'white',
+            lineHeight: '40px', textAlign: 'center'
+        }
+    }
     render() {
         return (
-            <div>
-                <div className="lay_auto">Hello World!</div>
-            </div>
+            <>
+                <div style={ this.styleBox }>
+                    <div className="lay_auto">
+                        <div style={{ padding: '15px' }}>
+                            <div style={ this.styleTitle }>방 검색</div>
+                            <div style={{ position: 'relative', height: '100px' }}>
+                                <div style={{ ...this.styleStation, ...this.styleStationL }}>
+                                    <div>출발</div>
+                                    <div>카이스트택시승강장</div>
+                                </div>
+                                <div>
+                                </div>
+                                <div style={{ ...this.styleStation, ...this.styleStationR }}>
+                                    <div>도착</div>
+                                    <div>대전역</div>
+                                </div>
+                            </div>
+                            <div style={ this.styleSearchBtn }>검색하기</div>
+                        </div>
+                    </div>
+                </div>
+            </>
         );
     }
 }
--- src/components/Setting/Setting.jsx
+++ src/components/Setting/Setting.jsx
@@ -1,11 +1,36 @@
+  
 import React, { Component } from 'react';
 
 class Setting extends Component {
+    constructor(props){
+        super(props);
+        this.styleBox = { background: 'white' }
+        this.styleProfBorder = {
+            position: 'absolute', left: '0px', top: '0px',
+            width: '60px', height: '60px', borderRadius: '30px',
+            overflow: 'hidden', border: '1px solid gray', background: 'white'
+        }
+        this.styleProf = {
+            width: '100%', height: '100%'
+        }
+    }
     render() {
         return (
-            <div>
-                <div className="lay_auto">개인 설정</div>
-            </div>
+            <>
+                <div style={ this.styleBox }>
+                    <div className="lay_auto">
+                        <div style={{ padding: '15px' }}>
+                            <div style={{ position: 'relative', height: '60px' }}>
+                                <div style={ this.styleProfBorder }>
+                                    <img style={ this.styleProf }/>
+                                </div>
+                                <div>김건</div>
+                                <div>20190052</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
         );
     }
 }
