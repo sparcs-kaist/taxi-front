325fa6376bbbe1b21ca949fc2dde68aba51a6061
14Kgun <geon6757@kaist.ac.kr>
2021-05-25 12:26:42 +0000

    Edit WhiteContainer.jsx

--- src/components/Frame/WhiteContainer/WhiteContainer.jsx
+++ src/components/Frame/WhiteContainer/WhiteContainer.jsx
@@ -4,15 +4,19 @@ class WhiteContainer extends Component {
     constructor(props){
         super(props);
         this.styleBox = { background: 'white' }
-        this.styleTitle = { fontSize: '22px', fontWeight: 900 }
+        this.styleTitle = {
+            marginBottom: '10px',
+            fontSize: '22px', fontWeight: 900
+        }
     }
     render(){
         return (
             <>
                 <div style={ this.styleBox }>
                     <div className="lay_auto">
-                        <div style={{ padding: '15px' }}>
+                        <div style={{ padding: '20px' }}>
                             <div style={ this.styleTitle }>{ this.props.title }</div>
+                            <div>{ this.props.children }</div>
                         </div>
                     </div>
                 </div>
