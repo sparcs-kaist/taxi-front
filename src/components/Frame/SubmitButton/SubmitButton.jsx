c13afcb3962d3711723ad49095be708293804a60
14Kgun <geon6757@kaist.ac.kr>
2021-09-13 23:54:23 +0900

    Add Frame/SubmitButton.jsx

--- src/components/Frame/SubmitButton/SubmitButton.jsx
+++ src/components/Frame/SubmitButton/SubmitButton.jsx
@@ -4,8 +4,9 @@ import { useSpring, animated } from 'react-spring';
 const Button = (props) => {
     const [isHover, setHover] = useState(false);
     const style = {
-        minHeight: '50px', textAlign: 'center',
-        fontSize: '16px', color: props.fontColor, fontWeight: 300
+        minHeight: '50px', lineHeight: '50px', textAlign: 'center',
+        fontSize: '16px', color: props.fontColor, fontWeight: 300,
+        borderRadius: '15px'
     }
     const background = useSpring({
         background: isHover ? props.backgroundHover : props.background,
@@ -14,8 +15,8 @@ const Button = (props) => {
     
     return (
         <div className="lay_auto ND">
-            <animated.div onMouseEnter={ () => setHover(true) } onMouseLeave={ () => setHover(false) }
-            style={{ ...style, ...background }}>{ props.children }</animated.div>
+            <animated.div className="BTNC" onMouseEnter={ () => setHover(true) } onMouseLeave={ () => setHover(false) }
+            onClick={ props.onClick } style={{ ...style, ...background }}>{ props.children }</animated.div>
         </div>
     );
 }
