d6abedca5a36ebf33b96df5fa5c7179c667fd6e8
michelle <michellekjs@kaist.ac.kr>
2021-09-27 20:11:46 +0900

    Fix time selection and parsing in searchpage

--- src/components/Search/SearchResult/SearchResult.jsx
+++ src/components/Search/SearchResult/SearchResult.jsx
@@ -9,11 +9,24 @@ export default class SearchResult extends Component {
         console.log(props.param);
     }
     render() {
+        let urlString = window.location.href
+        const parseList = urlString.split("#")
+        const roomName = parseList[0].split("/").pop()
+        const depPlace = decodeURIComponent(parseList[1])
+        const arrPlace = decodeURIComponent(parseList[2])
+        // const date = parseList[3] // date 가 아직 미완성
+        const depHour = parseList[3]
+        const depMin = parseList[4]
+    
         return (
             <div>
                 <div style={{ height: '20px' }}/>
                 <Title img={ svgSearch }>검색 결과</Title>
-                <div style={{ height: '20px' }}>{ window.location.href }</div>
+                <div>{roomName}</div>
+                <div>{depPlace}</div>
+                <div>{arrPlace}</div>
+                <div>{depHour}</div>
+                <div>{depMin}</div>
             </div>
         )
     }
