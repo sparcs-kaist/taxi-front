8b99f6a22d4ba57c2c0e3505349749d87ee0ce58
14Kgun <geon6757@kaist.ac.kr>
2021-11-15 21:39:25 +0900

    Update handleMessage

--- src/components/Chatting/Chatting.jsx
+++ src/components/Chatting/Chatting.jsx
@@ -6,24 +6,22 @@ import MessageForm from "./MessageForm";
 const Chatting = (props) => {
   const [newMessage, setNewMessage] = useState("");
 
-  //MessageForm 관련 함수들 - 시작-----
+  // MessageForm 관련 함수들 - 시작-----
   const handleNewMessageChange = (event) => {
     setNewMessage(event.target.value);
   };
   const handleSendMessage = (event) => {
     event?.preventDefault();
-    if (!newMessage) {
-      alert("내용을 입력하세요");
-    } else {
-      sendMessage(newMessage);
-    }
-
+    if (!newMessage) sendMessage(newMessage);
     setNewMessage("");
   };
   const sendMessage = (messageStr) => {
     alert("새 메시지: " + messageStr);
   };
   //MessageForm 관련 함수들 - 끝-------
+
+  // Events
+  
 
   const roomId = 0;
   return (
