ee5cab4a5a2dd0ff6489cba34fd5bb3003fd0e1a
Bread0288 <binlepain178@gmail.com>
2021-12-20 22:47:17 +0900

    save fixing moment

--- src/components/room/Myroom/Myroom.jsx
+++ src/components/room/Myroom/Myroom.jsx
@@ -1,55 +1,64 @@
-import React from "react";
-import { Component } from "react";
+import React, { useEffect, useState } from "react";
 import WhiteContainer from "../../Frame/WhiteContainer/WhiteContainer";
 import WhiteContainerMargin from "../../Frame/WhiteContainer/WhiteContainerMargin";
+
 import RoomElement from "./RoomElement";
+import RoomEntry from "../../Frame/RoomEntry/RoomEntry";
+
 import Footer from "../../Frame/Footer";
 import Title from "../../Frame/Title/Title";
-import RoomEntry from "../../Frame/RoomEntry/RoomEntry";
+
+import axios from "../../Tool/axios";
+
 import svgMyRoom from "../../Frame/NavigationIcon/chat_selected.svg";
 
-class Myroom extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="myroom">
-        <div style={{ height: "20px" }} />
-        <Title img={svgMyRoom}>내 방 리스트</Title>
-        <div style={{ height: "20px" }} />
+const Myroom = (props) => {
+  // const [myRoomList, setRoomList] = useState([]);
 
-        <WhiteContainer title="탑승 예정 택시" padding="11px">
-          <RoomEntry
-            title="서울 같이 가요~"
-            participants="2"
-            head="김넙죽"
-            from="택시승강장"
-            to="시외버스터미널"
-            date="2021년 7월 20일 09시 00분"
-          />
-        </WhiteContainer>
-        <WhiteContainer title="탑승 예정 택시" padding="11px">
-          <RoomEntry
-            title="둔산 갑니다"
-            participants="1"
-            head="박야옹"
-            from="택시승강장"
-            to="갤러리아"
-            date="2021년 7월 20일 17시 30분"
-          />
-        </WhiteContainer>
-        <WhiteContainer title="탑승 예정 택시" padding="11px">
-          <RoomEntry
-            title="대전역 ㄱ"
-            participants="3"
-            head="이거위"
-            from="오리연못"
-            to="대전역"
-            date="2021년 7월 20일 09시 30분"
-          />
-        </WhiteContainer>
-        {/* <WhiteContainer title="탑승 예정 택시">
+  useEffect(() => {
+    axios.get("/json/logininfo").then((res) => {
+      const userid = res.data;
+      console.log(userid);
+    });
+  });
+
+  return (
+    <div className="myroom">
+      <div style={{ height: "20px" }} />
+      <Title img={svgMyRoom}>내 방 리스트</Title>
+      <div style={{ height: "20px" }} />
+
+      <WhiteContainer title="탑승 예정 택시" padding="11px">
+        <RoomEntry
+          title="서울 같이 가요~"
+          participants="2"
+          head="김넙죽"
+          from="택시승강장"
+          to="시외버스터미널"
+          date="2021년 7월 20일 09시 00분"
+        />
+      </WhiteContainer>
+      <WhiteContainer title="탑승 예정 택시" padding="11px">
+        <RoomEntry
+          title="둔산 갑니다"
+          participants="1"
+          head="박야옹"
+          from="택시승강장"
+          to="갤러리아"
+          date="2021년 7월 20일 17시 30분"
+        />
+      </WhiteContainer>
+      <WhiteContainer title="탑승 예정 택시" padding="11px">
+        <RoomEntry
+          title="대전역 ㄱ"
+          participants="3"
+          head="이거위"
+          from="오리연못"
+          to="대전역"
+          date="2021년 7월 20일 09시 30분"
+        />
+      </WhiteContainer>
+      {/* <WhiteContainer title="탑승 예정 택시">
           <RoomElement
             title="12월 8일 오후 12시 택시팟 찾아요~"
             subtitle="대전역 > 카이스트, 오후 12시 30분 출발"
@@ -71,25 +80,24 @@ class Myroom extends Component {
             partImgs={[]}
           />
         </WhiteContainer> */}
-        <WhiteContainerMargin />
-        <WhiteContainer title="과거 탑승 택시">
-          <RoomElement
-            title="12월 8일 오후 12시 택시팟 찾아요~"
-            subtitle="대전역 > 카이스트, 오후 12시 30분 출발"
-            partLen={1}
-            partImgs={[]}
-          />
-          <RoomElement
-            title="술박스"
-            subtitle="카이스트 > 터미널, 오후 4시 0분 출발"
-            partLen={3}
-            partImgs={[]}
-          />
-        </WhiteContainer>
-        <Footer />
-      </div>
-    );
-  }
-}
+      <WhiteContainerMargin />
+      <WhiteContainer title="과거 탑승 택시">
+        <RoomElement
+          title="12월 8일 오후 12시 택시팟 찾아요~"
+          subtitle="대전역 > 카이스트, 오후 12시 30분 출발"
+          partLen={1}
+          partImgs={[]}
+        />
+        <RoomElement
+          title="술박스"
+          subtitle="카이스트 > 터미널, 오후 4시 0분 출발"
+          partLen={3}
+          partImgs={[]}
+        />
+      </WhiteContainer>
+      <Footer />
+    </div>
+  );
+};
 
 export default Myroom;
