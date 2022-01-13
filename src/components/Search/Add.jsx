ee1dcd40aee9ebbd8cfc3cd8dad937d5db25391e
michelle <michellekjs@kaist.ac.kr>
2022-01-13 13:54:19 +0900

    Making myroomlist

---
+++ src/components/Search/Add.jsx
@@ -0,0 +1,327 @@
+import React, { Component } from "react";
+import DatePicker from "../Frame/DatePicker/DatePicker";
+import RoomName from "../Frame/RoomName/RoomName";
+import RoomPlace from "../Frame/RoomPlace/RoomPlace";
+import RoomTime from "../Frame/RoomTime/RoomTime";
+import WhiteContainer from "../Frame/WhiteContainer/WhiteContainer.jsx";
+import Title from "../Frame/Title/Title";
+import SubmitButton from "../Frame/SubmitButton/SubmitButton";
+import SearchResult from "./SearchResult/SearchResult";
+
+import { Paper, Button } from "@material-ui/core";
+
+import svgSearch from "./svg_search.svg";
+import svgAddSelected from "./add_selected.svg";
+
+import PropTypes from "prop-types";
+
+import "react-datepicker/dist/react-datepicker.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Toast, ToastBody } from "react-bootstrap";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "../Tool/axios";
+
+class Add extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nameOpen: false,
+      placeOpen: false,
+      dateOpen: false,
+      timeOpen: false,
+      toastOpen: false,
+      toastMessage: undefined,
+      isResults: {
+        is: false,
+        data: [],
+      },
+      startDate: new Date(),
+      openTime: false,
+      roomName: undefined,
+      valueDate: [undefined, undefined],
+      valueGroupsDep: undefined,
+      valueGroupsArr: undefined,
+      valueGroupsTimeHour: undefined,
+      valueGroupsTimeMin: undefined,
+    };
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeDate = this.handleChangeDate.bind(this);
+    this.handleChangePlace = this.handleChangePlace.bind(this);
+    this.handleChangeTime = this.handleChangeTime.bind(this);
+    this.onClickSearch = this.onClickSearch.bind(this);
+    this.getAPIRes = this.getAPIRes.bind(this);
+    this.validateForm = this.validateForm.bind(this);
+  }
+
+  getAPIRes(dep, arr, startDate, name, part) {
+    if (this.props.isSearch) {
+      return axios.get("/rooms/search", {
+        params: {
+          from: dep,
+          to: arr,
+          time: startDate,
+        },
+      });
+    } else {
+      return axios.post("/rooms/create", {
+        data: {
+          name: name,
+          from: dep,
+          to: arr,
+          time: startDate,
+          part: part,
+        },
+      });
+    }
+  }
+
+  validateForm({ roomName, depString, arrString, date }) {
+    // 중복은 의도된 것, 추후 수정 필요
+    if (this.props.isSearch) {
+      let msg = "";
+      if (roomName === undefined || roomName === "")
+        msg = "방 이름을 입력해주세요.";
+      else if (depString === undefined || depString === "")
+        msg = "출발지를 입력해주세요.";
+      else if (arrString === undefined || arrString === "")
+        msg = "도착지를 입력해주세요.";
+      else if (depString === arrString)
+        msg = "출발지와 도착지는 같을 수 없습니다.";
+      else if (date[0] === undefined || date[1] === undefined)
+        msg = "날짜를 입력해주세요.";
+
+      return {
+        isValid: msg === "",
+        msg,
+      };
+    } else {
+      let msg = "";
+      if (roomName === undefined || roomName === "")
+        msg = "방 이름을 입력해주세요.";
+      else if (depString === undefined || depString === "")
+        msg = "출발지를 입력해주세요.";
+      else if (arrString === undefined || arrString === "")
+        msg = "도착지를 입력해주세요.";
+      else if (depString === arrString)
+        msg = "출발지와 도착지는 같을 수 없습니다.";
+      else if (date[0] === undefined || date[1] === undefined)
+        msg = "날짜를 입력해주세요.";
+
+      return {
+        isValid: msg === "",
+        msg,
+      };
+    }
+  }
+
+  handleChangeName(name) {
+    this.setState({
+      roomName: name,
+    });
+  }
+
+  handleChangePlace(dep, arr) {
+    this.setState({
+      valueGroupsDep: dep,
+      valueGroupsArr: arr,
+    });
+    // console.log(this.state.valueGroupsDep);
+  }
+
+  handleChangeDate(month, date) {
+    this.setState({
+      valueDate: [month, date],
+    });
+  }
+
+  handleChangeTime(hour, min) {
+    this.setState({
+      valueGroupsTimeHour: hour,
+      valueGroupsTimeMin: min,
+    });
+  }
+
+  handleButton() {
+    this.setState({ openName: !this.state.openName });
+  }
+
+  // 뭐에 대한 onclick?
+  async onClickSearch() {
+    const roomName = this.state.roomName;
+    const depString = this.state.valueGroupsDep;
+    const arrString = this.state.valueGroupsArr;
+    const depTimeString = this.state.valueGroupsTimeHour;
+    const arrTimeString = this.state.valueGroupsTimeMin;
+    const date = this.state.valueDate;
+
+    const formValidity = this.validateForm({
+      roomName,
+      depString,
+      arrString,
+      date,
+    });
+
+    if (formValidity.isValid) {
+      // date의 type 검증 필요
+      try {
+        const res = await this.getAPIRes(
+          depString,
+          arrString,
+          date,
+          roomName,
+          []
+        );
+        console.log(res);
+        if (res.status === 200) {
+          this.setState({
+            isResults: {
+              is: true,
+              data: res.data,
+            },
+          });
+        }
+      } catch (e) {
+        console.log("error occured while fetching API data");
+        console.log(e);
+      }
+    } else {
+      this.setState({ toastOpen: true, toastMessage: formValidity.msg });
+    }
+  }
+
+  render() {
+    const isResults = this.state.isResults;
+
+    return (
+      <>
+        {!isResults.is && (
+          <div className="searchroom">
+            <div style={{ height: "20px" }} />
+            {!this.props.isSearch && (
+              <>
+                <Title img={svgAddSelected}>
+                  {!this.props.isSearch && "방 만들기"}
+                </Title>
+                <div
+                  className="lay_auto ND"
+                  style={{
+                    position: "relative",
+                    paddingTop: "10px",
+                    paddingBottom: "10px",
+                    display: "flex",
+                    justifyContent: "flex-start",
+                  }}
+                >
+                  <Button
+                    style={{
+                      marginRight: "10px",
+                      borderRadius: "15px",
+                      backgroundColor: "white",
+                      boxShadow: "0px 1px 7.5px 2px rgba(0,0,0,0.05)",
+                      // backgroudColor: ,
+                    }}
+                    onClick={() =>
+                      this.setState({ nameOpen: !this.state.nameOpen })
+                    }
+                  >
+                    방 제목
+                  </Button>
+                  <Button
+                    style={{
+                      marginRight: "10px",
+                      borderRadius: "15px",
+                      backgroundColor: "white",
+                      boxShadow: "0px 1px 7.5px 2px rgba(0,0,0,0.05)",
+                    }}
+                    onClick={() =>
+                      this.setState({ placeOpen: !this.state.placeOpen })
+                    }
+                  >
+                    출발/도착지
+                  </Button>
+                  <Button
+                    style={{
+                      marginRight: "10px",
+                      borderRadius: "15px",
+                      backgroundColor: "white",
+                      boxShadow: "0px 1px 7.5px 2px rgba(0,0,0,0.05)",
+                    }}
+                    onClick={() =>
+                      this.setState({ dateOpen: !this.state.dateOpen })
+                    }
+                  >
+                    날짜
+                  </Button>
+                  <Button
+                    style={{
+                      borderRadius: "15px",
+                      backgroundColor: "white",
+                      boxShadow: "0px 1px 7.5px 2px rgba(0,0,0,0.05)",
+                    }}
+                    onClick={() =>
+                      this.setState({ timeOpen: !this.state.timeOpen })
+                    }
+                  >
+                    시각
+                  </Button>
+                </div>
+              </>
+            )}
+            <div style={{ height: "20px" }} />
+            {/* 방 제목으로 검색 */}
+            {this.state.nameOpen && (
+              <WhiteContainer title="방 검색">
+                <RoomName handler={this.handleChangeName} />
+              </WhiteContainer>
+            )}
+            {/* 출발지, 도착지로검색 */}
+            {this.state.placeOpen && (
+              <WhiteContainer title="장소">
+                <Paper style={{ height: "80px" }} elevation={0}>
+                  <RoomPlace handler={this.handleChangePlace} />
+                </Paper>
+              </WhiteContainer>
+            )}
+
+            {/* 날짜로 검색 */}
+            {this.state.dateOpen && (
+              <WhiteContainer title="날짜 검색">
+                <DatePicker handler={this.handleChangeDate} />
+              </WhiteContainer>
+            )}
+
+            {/* 시간으로 검색 */}
+            {this.state.timeOpen && (
+              <WhiteContainer title="시간">
+                <RoomTime handler={this.handleChangeTime}></RoomTime>
+              </WhiteContainer>
+            )}
+
+            <SubmitButton onClick={this.onClickSearch}>
+              {this.props.isSearch && "검색하기"}
+              {!this.props.isSearch && "방 만들기"}
+            </SubmitButton>
+            {this.state.toastOpen && (
+              <Toast
+                onClose={() => this.setState({ openToast: false })}
+                autohide
+                delay={3000}
+              >
+                <Toast.Header>Warning</Toast.Header>
+                <Toast.Body> {this.state.toastMessage}</Toast.Body>
+              </Toast>
+            )}
+          </div>
+        )}
+        {/* 지금은 그냥 방 추가일때도 이걸로 표시, 추후 내 방 리스트 프론트 만들어지면 그걸로 돌리면됨 */}
+        {isResults.is && <SearchResult searchResults={isResults.data} />}
+      </>
+    );
+  }
+}
+
+Add.propTypes = {
+  // FIXME specify type
+  isSearch: PropTypes.boolean,
+};
+export default Add;
--- src/components/room/Myroom/Myroom.jsx
+++ src/components/room/Myroom/Myroom.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import WhiteContainer from "../../Frame/WhiteContainer/WhiteContainer";
-import WhiteContainerMargin from "../../Frame/WhiteContainer/WhiteContainerMargin";
+// import BackgroundPurpleContainer from "../../Frame/BackgroundPurpleContainer/BackgroundPurpleContainer";
 
 import RoomElement from "./RoomElement";
 import RoomEntry from "../../Frame/RoomEntry/RoomEntry";
@@ -12,12 +12,15 @@ import axios from "../../Tool/axios";
 
 import svgMyRoom from "../../Frame/NavigationIcon/chat_selected.svg";
 
+// import "./Myroom.css";
+
 const Myroom = (props) => {
   const [myRoomList, setRoomList] = useState([]);
 
   const getUserRoom = async () => {
     const userRoom = await axios.get("rooms/searchByUser");
-    setRoomList(userRoom.data);
+    console.log(userRoom.data);
+    // setRoomList(userRoom.data);
   };
 
   useEffect(() => {
@@ -29,6 +32,19 @@ const Myroom = (props) => {
       <div style={{ height: "20px" }} />
       <Title img={svgMyRoom}>내 방 리스트</Title>
       <div style={{ height: "20px" }} />
+
+      {/* <div
+        className="lay_auto2 ND"
+        style={{ display: "flex", flexDirection: "row" }}
+      >
+        <div style={{ flex: "1 1 0" }}>
+          <WhiteContainer></WhiteContainer>
+        </div>
+        <div style={{ width: "15px" }}></div>
+        <div style={{ flex: "1 1 0" }}>
+          <WhiteContainer></WhiteContainer>
+        </div>
+      </div> */}
 
       {/* <WhiteContainer title="탑승 예정 택시" padding="11px">
         <RoomEntry
@@ -61,42 +77,45 @@ const Myroom = (props) => {
         />
       </WhiteContainer> */}
       {myRoomList.map((item, index) => (
-        <WhiteContainer key={index} title="탑승 예정 택시" padding="11px">
+        <WhiteContainer key={index} title="참여 중인 방" padding="11px">
           <RoomEntry
             title={item.name}
             participants={item.part.length}
-            head={item.part[0]}
+            head={item.part[0].nickname}
             from={item.from}
             to={item.to}
             date={item.time}
           />
         </WhiteContainer>
       ))}
-      {/* <WhiteContainer title="탑승 예정 택시">
-          <RoomElement
-            title="12월 8일 오후 12시 택시팟 찾아요~"
-            subtitle="대전역 > 카이스트, 오후 12시 30분 출발"
-            partLen={3}
-            partImgs={[]}
+      <WhiteContainer title="과거 참여 방" padding="20px">
+        <div className="subCategoryTitle">과거 참여 방</div>
+
+        <div className="dashedLine"></div>
+
+        {/* <BackgroundPurpleContainer title="_" padding="11px">
+          <RoomEntry
+            title="서울 같이 가요~"
+            participants={2}
+            head="김넙죽"
+            from="택시승강장"
+            to="시외버스터미널"
+            date="2021년 7월 20일 오전 9시 00분"
           />
-        </WhiteContainer>
-        <WhiteContainer title="탑승 예정 택시">
-          <RoomElement
-            title="12월 8일 오후 12시 택시팟 찾아요~"
-            subtitle="대전역 > 카이스트, 오후 12시 30분 출발"
-            partLen={4}
-            partImgs={[]}
+        </BackgroundPurpleContainer>
+
+        <BackgroundPurpleContainer title="_" padding="11px">
+          <RoomEntry
+            title="둔산 갑니다"
+            participants={1}
+            head="박야옹"
+            from="택시승강장"
+            to="갤러리아"
+            date="2021년 7월 20일 오후 5시 30분"
           />
-          <RoomElement
-            title="술박스"
-            subtitle="카이스트 > 터미널, 오후 4시 0분 출발"
-            partLen={5}
-            partImgs={[]}
-          />
-        </WhiteContainer> */}
-      <WhiteContainerMargin />
-      <WhiteContainer title="과거 탑승 택시">
-        <RoomElement
+        </BackgroundPurpleContainer> */}
+
+        {/* <RoomElement
           title="12월 8일 오후 12시 택시팟 찾아요~"
           subtitle="대전역 > 카이스트, 오후 12시 30분 출발"
           partLen={1}
@@ -107,7 +126,7 @@ const Myroom = (props) => {
           subtitle="카이스트 > 터미널, 오후 4시 0분 출발"
           partLen={3}
           partImgs={[]}
-        />
+        /> */}
       </WhiteContainer>
       <Footer />
     </div>
