8d82e660b9e9eb4fc74da807c074b4ec3aef5a6e
Park Jinho <binlepain178@gmail.com>
2022-02-14 09:48:02 +0900

    fix BackgroundPurpleContainer to auto-height

--- src/components/Frame/BackgroundPurpleContainer/BackgroundPurpleContainer.jsx
+++ src/components/Frame/BackgroundPurpleContainer/BackgroundPurpleContainer.jsx
@@ -8,23 +8,43 @@ class BackgroundPurpleContainer extends Component {
         style={
           this.props.isSelected
             ? {
+                position: "relative",
                 marginTop: "20px",
                 boxShadow:
                   "0px 2px 4px rgba(110, 54, 120, 0.2), 0px 1px 18px rgba(110, 54, 120, 0.12), 0px 6px 10px rgba(110, 54, 120, 0.14)",
                 borderRadius: "6px",
                 overflow: "hidden",
-                height: "109px",
               }
             : {
+                position: "relative",
                 marginTop: "20px",
                 boxShadow:
                   "0px 1.5px 1px -0.5px rgba(110, 54, 120, 0.04), 0px 2.5px 1px -0.5px rgba(110, 54, 120, 0.02), 0px 2px 3px -1px rgba(110, 54, 120, 0.1)",
                 borderRadius: "6px",
                 overflow: "hidden",
-                height: "109px",
               }
         }
       >
+        <div
+          style={
+            this.props.isSelected
+              ? {
+                  zIndex: "2",
+                  position: "absolute",
+                  width: "8px",
+                  height: "100%",
+                  left: "0px",
+                  background: "#6E3678",
+                }
+              : {
+                  position: "absolute",
+                  zIndex: "2",
+                  width: "8px",
+                  height: "100%",
+                  left: "0px",
+                }
+          }
+        ></div>
         <div
           style={{
             position: "relative",
@@ -34,26 +54,6 @@ class BackgroundPurpleContainer extends Component {
         >
           {this.props.children}
         </div>
-        <div
-          style={
-            this.props.isSelected
-              ? {
-                  position: "relative",
-                  width: "8px",
-                  height: "109px",
-                  left: "0px",
-                  top: "-109px",
-                  background: "#6E3678",
-                }
-              : {
-                  position: "relative",
-                  width: "8px",
-                  height: "120px",
-                  left: "0px",
-                  top: "-120px",
-                }
-          }
-        ></div>
       </div>
     );
   }
