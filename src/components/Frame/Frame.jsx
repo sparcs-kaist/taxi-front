4d0b6370d2c49702ca3b07dbca85b6f1057c9d0b
14Kgun <geon6757@kaist.ac.kr>
2022-04-12 22:54:44 +0900

    Update Frame

--- src/components/Frame/Frame.jsx
+++ src/components/Frame/Frame.jsx
@@ -1,16 +1,11 @@
-// 최상위 컴포넌트
-// 로그인 여부를 체크하고, 로그인이 되어있지 않으면 로그인 페이지로 리다이렉트
-// props 주석은 최하단 참조
-
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import axios from "../Tool/axios";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
-import "./Frame.css";
 import PropTypes from "prop-types";
 
-const TopBar = (props) => {
+const HeaderLine = () => {
   return (
     <div
       style={{
@@ -25,51 +20,48 @@ const TopBar = (props) => {
   );
 };
 
-/*const Frame = (props) => {
+const Frame = (props) => {
+  const [userId, setUserId] = useState(undefined);
+  const currentPath = window.location.pathname;
 
-}*/
+  const styleContainer = {
+    width: "100%",
+    height: "100%",
+    position: "relative",
+  };
 
-class Frame extends Component {
-  constructor(props) {
-    // constructor 에서 빼기
-    super(props);
-
-    this.state = { loginCheck: undefined };
+  // path가 수정될 때 마다 logininfo 요청
+  useEffect(() => {
     axios
       .get("/json/logininfo")
-      .then((userInfo) => {
-        if (userInfo.data.id) this.setState({ loginCheck: true });
-        else this.setState({ loginCheck: false });
+      .then(({ data }) => {
+        setUserId(data.id ? data.id : null);
       })
       .catch((error) => {
-        console.log("axios error : " + error); // FIXME: 추후 수정 바람
+        console.log("Frame error : " + error); // FIXME: 추후 수정 바람
       });
+  }, [currentPath]);
+
+  if (userId === undefined) {
+    return (
+      <div style={styleContainer}>
+        <Navigation selected={props.navi} />
+        <HeaderLine />
+      </div>
+    );
+  } else if (userId === null) {
+    return <Redirect to={"/login?redirect=" + window.location.pathname} />;
+  } else {
+    return (
+      <div style={styleContainer}>
+        {props.children}
+        <Footer />
+        <Navigation selected={props.navi} />
+        <HeaderLine />
+      </div>
+    );
   }
-  render() {
-    if (this.state.loginCheck === undefined) {
-      // 백 서버 offline인 경우 (FIXME)
-      return (
-        <div id="main-container">
-          <Navigation selected={this.props.navi} />
-          <TopBar />
-        </div>
-      );
-    } else if (this.state.loginCheck === false) {
-      // 로그인이 되어 있지 않은 경우(백 서버 online), 로그인 페이지로 리다이렉트
-      return <Redirect to={"/login?redirect=" + window.location.pathname} />;
-    } else {
-      // 로그인이 되어있는 경우
-      return (
-        <div id="main-container">
-          {this.props.children}
-          <Footer />
-          <Navigation selected={this.props.navi} />
-          <TopBar />
-        </div>
-      );
-    }
-  }
-}
+};
 
 Frame.propTypes = {
   navi: PropTypes.string,
