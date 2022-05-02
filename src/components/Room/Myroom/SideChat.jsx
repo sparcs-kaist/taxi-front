73cd977b90bf46dda09dd5969033029f57d01815
TaewooKim <rlaxodntttt@kaist.ac.kr>
2022-05-02 14:46:18 +0900

    make test jsx part

--- src/components/Room/Myroom/SideChat.jsx
+++ src/components/Room/Myroom/SideChat.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-
+import Chatting from "../../Chatting/Chatting"
 const SideChat = (props) => {
-  return <div>{props.id}</div>;
+  return (
+    <div>
+      {props.id}
+      <div>
+        <Chatting/>
+      </div>
+    </div>
+  );
 };
 SideChat.propTypes = {
   id: PropTypes.string,
