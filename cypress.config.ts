b5ff2cb3eb8555fe8d949f3dbbc50036bfd4a818
Jinwoo Lee <starday456@kaist.ac.kr>
2022-07-21 00:27:05 +0900

    add baseUrl and viewport size

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,10 +6,14 @@ export default defineConfig({
       root: "#root",
     },
   },
-
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
+
+  // iPhone X
+  viewportWidth: 414,
+  viewportHeight: 896,
 });
