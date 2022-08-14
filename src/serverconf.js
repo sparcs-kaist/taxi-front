89762b2104254d75f9724bc78498843d5014cb31
withsang <jsa5115@naver.com>
2022-08-14 21:48:43 +0900

    Fix: Separate Socket.io server url from backend url

--- src/serverconf.js
+++ src/serverconf.js
@@ -2,5 +2,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const backServer = process.env.REACT_APP_BACK_URL;
+const ioServer = process.env.REACT_APP_IO_URL ?? backServer;
 
-export { backServer };
+export { backServer, ioServer };
