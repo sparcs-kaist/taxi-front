b5c9525c90796b134c0d4d04fdae7388716f012a
Jinwoo Lee <starday456@kaist.ac.kr>
2022-08-16 18:32:52 +0900

    Add: set room information on click Room

--- src/components/Search/SideResult.jsx
+++ src/components/Search/SideResult.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import WhiteContainer from "components/common/WhiteContainer";
 import Title from "components/common/Title";
 import Room from "components/common/room/Room";
+import RoomSelectionModal from "./RoomSelectionModal";
 import PropTypes from "prop-types";
 
 const SideResult = (props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedRoomInfo, setSelectedRoomInfo] = useState(null);
+
   const styleEmpty = {
     color: "#888888",
     fontWeight: "700",
@@ -15,6 +19,15 @@ const SideResult = (props) => {
   if (!props.mobile) {
     return (
       <div style={{ marginTop: 26 }}>
+        <RoomSelectionModal
+          isOpen={isModalOpen}
+          isMobile={false}
+          onClose={() => {
+            setIsModalOpen(false);
+            setSelectedRoomInfo(null);
+          }}
+          roomInfo={selectedRoomInfo}
+        />
         <WhiteContainer marginAuto={false} padding="20px 20px 22px">
           <Title icon="search_result" marginAuto={false}>
             검색 결과
@@ -28,6 +41,10 @@ const SideResult = (props) => {
                 marginTop="15px"
                 mobile={props.mobile}
                 data={room}
+                onClick={() => {
+                  setIsModalOpen(!isModalOpen);
+                  setSelectedRoomInfo(room);
+                }}
               />
             ))
           )}
@@ -37,6 +54,15 @@ const SideResult = (props) => {
   } else {
     return (
       <>
+        <RoomSelectionModal
+          isOpen={isModalOpen}
+          isMobile={true}
+          onClose={() => {
+            setIsModalOpen(false);
+            setSelectedRoomInfo(null);
+          }}
+          roomInfo={selectedRoomInfo}
+        />
         {props.result.length == 0 ? (
           <WhiteContainer marginAuto={false} style={styleEmpty}>
             <div style={styleEmpty}>검색 결과가 없습니다</div>
@@ -51,6 +77,10 @@ const SideResult = (props) => {
                 marginTop="0px"
                 marginBottom="15px"
                 mobile={props.mobile}
+                onClick={() => {
+                  setIsModalOpen(!isModalOpen);
+                  setSelectedRoomInfo(room);
+                }}
               />
             );
           })
