db4b2ab7ad3f6c5dc18a49780ab522d7d354d038
14Kgun <geon6757@kaist.ac.kr>
2022-08-19 04:55:19 +0900

    Add: Channel Talk Support Btn on Mypage

---
+++ src/components/Skeleton/ChannelTalk.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import ChannelService from "tools/channelService";
+import { channelTalkPluginKey } from "serverconf";
+
+const ChannelTalk = () => {
+  useEffect(() => {
+    ChannelService.boot({
+      pluginKey: channelTalkPluginKey, //please fill with your plugin key
+      hideChannelButtonOnBoot: true,
+      customLauncherSelector: ".popup-channeltalk",
+    });
+    return () => {
+      ChannelService.shutdown();
+    };
+  });
+  return null;
+};
+
+export default ChannelTalk;
