db969fde18537a70d1995ec2c43528c2fe56c01b
Jinwoo Lee <starday456@kaist.ac.kr>
2022-08-22 21:13:03 +0900

    Add: location atom and selector - selector는 언어 설정에 따라 다른 location name을 반환

---
+++ src/recoil/taxiLocation/index.js
@@ -0,0 +1,6 @@
+import taxiLocationAtom from "./atom";
+import taxiLocataionWithName from "./withName";
+
+export { taxiLocataionWithName };
+
+export default taxiLocationAtom;
---
+++ src/recoil/taxiLocation/withName.js
@@ -0,0 +1,17 @@
+import { selector } from "recoil";
+import taxiLocationAtom from "./atom";
+import preferenceAtom from "../preference";
+
+const taxiLocataionWithName = selector({
+  key: "locationWithName",
+  get: ({ get }) =>
+    get(taxiLocationAtom).reduce((acc, place) => {
+      acc.push({
+        ...place,
+        name: get(preferenceAtom).lang === "ko" ? place.koName : place.enName,
+      });
+      return acc;
+    }, []),
+});
+
+export default taxiLocataionWithName;
