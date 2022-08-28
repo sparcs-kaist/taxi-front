82dad163be9413dc87b8da875f8235b52c62f89d
14Kgun <geon6757@kaist.ac.kr>
2022-08-29 08:46:06 +0900

    Style: AlertProvider

--- src/components/Skeleton/AlertProvider.jsx
+++ src/components/Skeleton/AlertProvider.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Modal from "components/common/modal/Modal";
 import { useRecoilState } from "recoil";
 import alertAtom from "recoil/alert";
+import Button from "components/common/modal/Button";
 
 const AlertProvider = () => {
   const [message, setMessage] = useRecoilState(alertAtom);
@@ -12,9 +13,20 @@ const AlertProvider = () => {
       onClickClose={() => setMessage(null)}
       maxWidth="325px"
       padding="10px"
+      top="100px"
       btnCloseDisplay={true}
     >
-      {message}
+      <div style={{ padding: "10px", paddingBottom: "20px", fontSize: "15px" }}>
+        {message}
+      </div>
+      <Button
+        onClick={() => setMessage(null)}
+        boxShadow="inset 2px 2px 5px -2px rgba(0, 0, 0, 0.25)"
+        color="#FFFFFF"
+        background="#6E3678"
+      >
+        확인
+      </Button>
     </Modal>
   );
 };
