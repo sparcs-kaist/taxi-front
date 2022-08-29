64a05fd6201f8a6507fee6a4cc9b70b5a0b31523
14Kgun <geon6757@kaist.ac.kr>
2022-08-29 18:38:49 +0900

    Refcator: App.css

--- src/components/Skeleton/Skeleton.jsx
+++ src/components/Skeleton/Skeleton.jsx
@@ -31,6 +31,7 @@ const HeaderLine = () => {
           position: "fixed",
           top: "0px",
           left: "0px",
+          zIndex: 30,
         }}
       />
       <div style={{ height: "max(0px, env(safe-area-inset-top))" }} />
