00889544c80211912284a9901943155bc35ecd00
14Kgun <geon6757@kaist.ac.kr>
2022-08-29 09:00:48 +0900

    Refactor: AlertProvider

--- src/components/Skeleton/AlertProvider.jsx
+++ src/components/Skeleton/AlertProvider.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useStateWithCallbackLazy } from "use-state-with-callback";
 import Modal from "components/common/modal/Modal";
 import { useRecoilState } from "recoil";
 import alertAtom from "recoil/alert";
 import Button from "components/common/modal/Button";
 
 const AlertProvider = () => {
+  const [display, setDisplay] = useStateWithCallbackLazy(false);
   const [message, setMessage] = useRecoilState(alertAtom);
+
+  useEffect(() => setDisplay(message ? true : false), [message]);
+  const onClickClose = () => {
+    setDisplay(false, () => setMessage(null));
+  };
 
   return (
     <Modal
-      display={message ? true : false}
-      onClickClose={() => setMessage(null)}
+      display={display}
+      onClickClose={onClickClose}
       maxWidth="325px"
       padding="10px"
       top="100px"
@@ -20,7 +27,7 @@ const AlertProvider = () => {
         {message}
       </div>
       <Button
-        onClick={() => setMessage(null)}
+        onClick={onClickClose}
         boxShadow="inset 2px 2px 5px -2px rgba(0, 0, 0, 0.25)"
         color="#FFFFFF"
         background="#6E3678"
