2ad83fead2e472aabe7c693a971632af3665fb69
imYourChoi <jeehun@kaist.ac.kr>
2022-09-15 01:49:20 +0900

    Add color to theme.js

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,7 +1,21 @@
 export const theme = {
   black: "#323232",
   white: "#FFFFFF",
+
   purple: "#6E3678",
-  light_purple: "#FAF6FB",
-  background_purple: "#FAF8FB",
+  purple_disabled: "#B89DBD",
+  purple_dark: "#572A5E",
+  purple_light: "#FAF6FB",
+  purple_background: "#FAF8FB",
+
+  gray_text: "#888888",
+  gray_background: "EEEEEE",
+  gray_line: "#C8C8C8",
+
+  red_text: "#DD616E",
+  red_button: "#91313B",
+  red_background: "#F9E8E7",
+
+  green_button: "#23913C",
+  green_background: "#E6F7E4",
 };
