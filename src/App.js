43d5c0dd3a8b517650d808468932c5b4c041751b
antaechan <0627antaechan@gmail.com>
2022-09-19 23:33:00 +0900

    Add lang folder and json files for translation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import ScrollRestoration from "react-scroll-restoration";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+
 import ChannelTalk from "components/Skeleton/ChannelTalk";
 import AlertProvider from "components/Skeleton/AlertProvider";
 import Skeleton from "components/Skeleton/Skeleton";
@@ -14,8 +17,38 @@ import Mypage from "components/Mypage/Mypage";
 import WrapChat from "components/Chatting/WrapChat";
 import Error from "components/Error/Error";
 
+import TranslationEn from "./translation.en.json";
+import TranslationKo from "./translation.ko.json";
+
 import "App.css";
 import "Font.css";
+
+const resource = {
+  en: {
+    translations: TranslationEn,
+  },
+  ko: {
+    translations: TranslationKo,
+  },
+};
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources: resource,
+    lng: "ko", // if you're using a language detector, do not define the lng option
+    fallbackLng: "ko",
+    debug: true,
+    defaultNS: "translations",
+    ns: "translations",
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    },
+  });
 
 const App = () => {
   return (
