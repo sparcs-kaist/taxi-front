5351ddec38556941a0266b0365cd581d09154264
imYourChoi <jeehun@kaist.ac.kr>
2022-09-27 12:54:21 +0900

    Reflect comments from GrasshopperBears

--- src/components/common/DottedLine.tsx
+++ src/components/common/DottedLine.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { theme } from "styles/theme";
 
 type Direction = "row" | "column";
 
@@ -8,16 +9,20 @@ type LineProps = {
 
 const DottedLine = ({ direction }: LineProps) => {
   const Wrapper = {
-    height: direction == "row" ? 1 : "100%",
-    width: direction == "row" ? "100%" : 1,
+    height: direction === "row" ? 1 : "100%",
+    width: direction === "row" ? "100%" : 1,
   };
   const line = {
-    height: direction == "row" ? undefined : "calc(100% + 5px)",
-    width: direction == "row" ? "calc(100% + 5px)" : undefined,
-    borderLeft: direction == "row" ? undefined : "5px dotted #C8C8C8",
-    borderTop: direction == "row" ? "5px dotted #C8C8C8" : undefined,
-    marginLeft: direction == "row" ? -2.5 : undefined,
-    marginTop: direction == "row" ? undefined : -2.5,
+    height: direction === "row" ? undefined : "calc(100% + 5px)",
+    width: direction === "row" ? "calc(100% + 5px)" : undefined,
+    // Row direction
+    borderTop:
+      direction === "row" ? `5px dotted ${theme.gray_line}` : undefined,
+    marginLeft: direction === "row" ? -2.5 : undefined,
+    // Column direction
+    borderLeft:
+      direction === "column" ? `5px dotted ${theme.gray_line}` : undefined,
+    marginTop: direction === "column" ? -2.5 : undefined,
   };
   return (
     <div style={{ ...Wrapper, overflow: "hidden" }}>
