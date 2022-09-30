2ab5f780968aba460f79c19d612dc825bf6b3b78
imYourChoi <jeehun@kaist.ac.kr>
2022-09-30 12:25:03 +0900

    Reset marginAuto

--- src/components/common/WhiteContainer.jsx
+++ src/components/common/WhiteContainer.jsx
@@ -41,7 +41,7 @@ WhiteContainer.propTypes = {
 WhiteContainer.defaultProps = {
   padding: "24px",
   marginBottom: "15px",
-  marginAuto: false,
+  marginAuto: true,
 };
 
 export default WhiteContainer;
