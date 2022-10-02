381122f4db302ccfbab1e1502b8e72369bb833da
Jinwoo Lee <starday456@kaist.ac.kr>
2022-10-02 16:23:35 +0900

    Add: Myroom 페이지에 pagination 정보, handler 추가

--- src/components/Myroom/Myroom.jsx
+++ src/components/Myroom/Myroom.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { useR2state } from "hooks/useReactiveState";
 import useTaxiAPI from "hooks/useTaxiAPI";
 import R1Myroom from "./R1Myroom";
 import R2Myroom from "./R2Myroom";
+
+const PAGE_MAX_ROOMS = 20;
 
 const Myroom = () => {
   const history = useHistory();
@@ -13,10 +15,67 @@ const Myroom = () => {
   const [, roomList] = useTaxiAPI.get("/rooms/v2/searchByUser", {}, [
     roomListToken,
   ]);
+  const [ongoingPageInfo, setOngoingPageInfo] = useState({
+    totalPages: 1,
+    currentPage: 1,
+  });
+  const [donePageInfo, setDonePageInfo] = useState({
+    totalPages: 1,
+    currentPage: 1,
+  });
 
   if (reactiveState == 3 && roomId) {
     history.replace(`/chatting/${roomId}`);
   }
+
+  const ongoingPageClickHandler = (page) => {
+    setOngoingPageInfo({ ...ongoingPageInfo, currentPage: page });
+  };
+  const donePageClickHandler = (page) => {
+    setDonePageInfo({ ...donePageInfo, currentPage: page });
+  };
+
+  const ongoingPrevPageHandler = () => {
+    if (ongoingPageInfo.currentPage <= 1) return;
+    setOngoingPageInfo({
+      ...ongoingPageInfo,
+      currentPage: ongoingPageInfo.currentPage - 1,
+    });
+  };
+  const donePrevPageHandler = () => {
+    if (donePageInfo.currentPage <= 1) return;
+    setDonePageInfo({
+      ...donePageInfo,
+      currentPage: donePageInfo.currentPage - 1,
+    });
+  };
+
+  const ongoingNextPageHandler = () => {
+    if (ongoingPageInfo.currentPage >= ongoingPageInfo.totalPages) return;
+    setOngoingPageInfo({
+      ...ongoingPageInfo,
+      currentPage: ongoingPageInfo.currentPage + 1,
+    });
+  };
+  const doneNextPageHandler = () => {
+    if (donePageInfo.currentPage >= donePageInfo.totalPages) return;
+    setDonePageInfo({
+      ...donePageInfo,
+      currentPage: donePageInfo.currentPage + 1,
+    });
+  };
+
+  useEffect(() => {
+    if (!roomList) return;
+    setOngoingPageInfo({
+      totalPages: Math.ceil(roomList.ongoing.length / PAGE_MAX_ROOMS),
+      currentPage: 1,
+    });
+    setDonePageInfo({
+      totalPages: Math.ceil(roomList.done.length / PAGE_MAX_ROOMS),
+      currentPage: 1,
+    });
+  }, [roomList]);
 
   return reactiveState === 3 ? (
     <R1Myroom
@@ -24,6 +83,14 @@ const Myroom = () => {
       ongoing={roomList?.ongoing}
       done={roomList?.done}
       recallEvent={() => setRoomListToken(Date.now().toString())}
+      ongoingPageInfo={ongoingPageInfo}
+      donePageInfo={donePageInfo}
+      ongoingPageClickHandler={ongoingPageClickHandler}
+      donePageClickHandler={donePageClickHandler}
+      ongoingNextPageHandler={ongoingNextPageHandler}
+      doneNextPageHandler={doneNextPageHandler}
+      ongoingPrevPageHandler={ongoingPrevPageHandler}
+      donePrevPageHandler={donePrevPageHandler}
     />
   ) : (
     <R2Myroom
@@ -31,6 +98,14 @@ const Myroom = () => {
       ongoing={roomList?.ongoing}
       done={roomList?.done}
       recallEvent={() => setRoomListToken(Date.now().toString())}
+      ongoingPageInfo={ongoingPageInfo}
+      donePageInfo={donePageInfo}
+      ongoingPageClickHandler={ongoingPageClickHandler}
+      donePageClickHandler={donePageClickHandler}
+      ongoingNextPageHandler={ongoingNextPageHandler}
+      doneNextPageHandler={doneNextPageHandler}
+      ongoingPrevPageHandler={ongoingPrevPageHandler}
+      donePrevPageHandler={donePrevPageHandler}
     />
   );
 };
