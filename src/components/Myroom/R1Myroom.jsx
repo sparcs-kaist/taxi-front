de7f6f8edc02e02a11e61d045ad09c2929dbf50c
Jinwoo Lee <starday456@kaist.ac.kr>
2022-10-02 16:23:51 +0900

    Add: R1 레이아웃에 Pagination 컴포넌트 추가

--- src/components/Myroom/R1Myroom.jsx
+++ src/components/Myroom/R1Myroom.jsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 import Title from "components/common/Title";
 import Room from "components/common/room/Room";
 import RLayout from "components/common/RLayout";
+import Pagination from "components/common/pagination/Pagination";
 import PropTypes from "prop-types";
+
+const PAGE_MAX_ROOMS = 20;
 
 const R1Myroom = (props) => {
   const styleEmpty = {
@@ -22,20 +25,35 @@ const R1Myroom = (props) => {
       {props.ongoing.length === 0 ? (
         <div style={styleEmpty}>참여 중인 방이 없습니다.</div>
       ) : (
-        props.ongoing.map((item) => (
-          <Link
-            key={item._id}
-            to={`/myroom/${item._id}`}
-            style={{ textDecoration: "none" }}
-          >
-            <Room
-              data={item}
-              selected={props.roomId === item._id}
-              theme="white"
-              marginBottom="15px"
-            />
-          </Link>
-        ))
+        <>
+          {props.ongoing
+            .slice(
+              PAGE_MAX_ROOMS * (props.ongoingPageInfo.currentPage - 1),
+              PAGE_MAX_ROOMS * props.ongoingPageInfo.currentPage
+            )
+            .map((item) => (
+              <Link
+                key={item._id}
+                to={`/myroom/${item._id}`}
+                style={{ textDecoration: "none" }}
+              >
+                <Room
+                  data={item}
+                  selected={props.roomId === item._id}
+                  theme="white"
+                  marginBottom="15px"
+                />
+              </Link>
+            ))}
+          <Pagination
+            totalPages={props.ongoingPageInfo.totalPages}
+            currentPage={props.ongoingPageInfo.currentPage}
+            onClickPage={props.ongoingPageClickHandler}
+            onClickPrev={props.ongoingPrevPageHandler}
+            onClickNext={props.ongoingNextPageHandler}
+            isMobile
+          />
+        </>
       )}
       <Title icon="past" header={true}>
         과거 참여 방
@@ -43,20 +61,35 @@ const R1Myroom = (props) => {
       {props.done.length === 0 ? (
         <div style={styleEmpty}>과거 참여했던 방이 없습니다.</div>
       ) : (
-        props.done.map((item) => (
-          <Link
-            key={item._id}
-            to={`/myroom/${item._id}`}
-            style={{ textDecoration: "none" }}
-          >
-            <Room
-              data={item}
-              selected={props.roomId === item._id}
-              theme="white"
-              marginTop="15px"
-            />
-          </Link>
-        ))
+        <>
+          {props.done
+            .slice(
+              PAGE_MAX_ROOMS * (props.donePageInfo.currentPage - 1),
+              PAGE_MAX_ROOMS * props.donePageInfo.currentPage
+            )
+            .map((item) => (
+              <Link
+                key={item._id}
+                to={`/myroom/${item._id}`}
+                style={{ textDecoration: "none" }}
+              >
+                <Room
+                  data={item}
+                  selected={props.roomId === item._id}
+                  theme="white"
+                  marginTop="15px"
+                />
+              </Link>
+            ))}
+          <Pagination
+            totalPages={props.donePageInfo.totalPages}
+            currentPage={props.donePageInfo.currentPage}
+            onClickPage={props.donePageClickHandler}
+            onClickPrev={props.donePrevPageHandler}
+            onClickNext={props.doneNextPageHandler}
+            isMobile
+          />
+        </>
       )}
     </RLayout.R1>
   );
@@ -66,6 +99,14 @@ R1Myroom.propTypes = {
   roomId: PropTypes.string,
   ongoing: PropTypes.array,
   done: PropTypes.array,
+  ongoingPageInfo: PropTypes.object,
+  donePageInfo: PropTypes.object,
+  ongoingPageClickHandler: PropTypes.func,
+  donePageClickHandler: PropTypes.func,
+  ongoingNextPageHandler: PropTypes.func,
+  doneNextPageHandler: PropTypes.func,
+  ongoingPrevPageHandler: PropTypes.func,
+  donePrevPageHandler: PropTypes.func,
 };
 R1Myroom.defaultProps = {
   ongoing: [],
