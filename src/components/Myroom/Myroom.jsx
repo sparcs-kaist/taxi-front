702af61508a05c1667fce99b943689c3111a36a3
Jinwoo Lee <starday456@kaist.ac.kr>
2022-10-14 21:01:39 +0900

    Refactor: 페이지 파싱 hook 적용

--- src/components/Myroom/Myroom.jsx
+++ src/components/Myroom/Myroom.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { useHistory, useParams, useLocation } from "react-router-dom";
-import qs from "qs";
+import { useHistory, useParams } from "react-router-dom";
 
 import { useR2state } from "hooks/useReactiveState";
 import useTaxiAPI from "hooks/useTaxiAPI";
+import usePageFromSearchParams from "hooks/usePageFromSearchParams";
 import R1Myroom from "./R1Myroom";
 import R2Myroom from "./R2Myroom";
 
@@ -11,7 +11,6 @@ const PAGE_MAX_ROOMS = 20;
 
 const Myroom = () => {
   const history = useHistory();
-  const location = useLocation();
   const { roomId } = useParams();
   const reactiveState = useR2state();
   const [roomListToken, setRoomListToken] = useState(Date.now().toString());
@@ -22,6 +21,7 @@ const Myroom = () => {
     totalPages: 1,
     currentPage: 1,
   });
+  const { page, isValid: isValidPage } = usePageFromSearchParams();
 
   if (reactiveState == 3 && roomId) {
     history.replace(`/chatting/${roomId}`);
@@ -29,14 +29,12 @@ const Myroom = () => {
 
   useEffect(() => {
     if (!roomList) return;
-    const q = qs.parse(location.search.slice(1));
-    const currentPage = Number(q.page);
 
     setDonePageInfo({
       totalPages: Math.ceil(roomList.done.length / PAGE_MAX_ROOMS),
-      currentPage: Number.isNaN(currentPage) ? 1 : currentPage,
+      currentPage: isValidPage ? page : 1,
     });
-  }, [roomList, location]);
+  }, [roomList, page, isValidPage]);
 
   return reactiveState === 3 ? (
     <R1Myroom
