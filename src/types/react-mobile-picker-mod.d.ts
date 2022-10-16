3f5cf1af120e2026f24a14636367822222c2d2ed
Jinwoo Lee <starday456@kaist.ac.kr>
2022-10-17 01:40:21 +0900

    Add: Time Picker 타입 정의

---
+++ src/types/react-mobile-picker-mod.d.ts
@@ -0,0 +1,16 @@
+declare module "react-mobile-picker-mod" {
+  import React from "react";
+
+  type OptionGroupsType = { hour: number[] } | { min: number[] };
+  type ValueType = { hour: number } | { min: number };
+
+  type PickerProps = {
+    optionGroups: optionGroupsType;
+    valueGroups: ValueType;
+    onChange: (key: string, value: number) => void;
+    itemHeight: number;
+    height: number;
+  };
+
+  export default function Picker(props: PickerProps): JSX;
+}
