1a769151cb3b6163c30dbd17142754f23715dc02
Jinwoo Lee <starday456@kaist.ac.kr>
2022-10-17 01:58:01 +0900

    Refactor: 타입 추가

--- src/components/common/roomOptions/MaxPart.tsx
+++ src/components/common/roomOptions/MaxPart.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { CSSProperties } from "react";
 import WhiteContainer from "components/common/WhiteContainer";
 import { theme } from "styles/theme";
 
@@ -7,12 +6,24 @@ import PeopleRoundedIcon from "@material-ui/icons/PeopleRounded";
 import RemoveRoundedIcon from "@material-ui/icons/RemoveRounded";
 import AddRoundedIcon from "@material-ui/icons/AddRounded";
 
-const Counter = (props) => {
+type CounterProps = {
+  min: number;
+  max: number;
+  number: number;
+  setNumber: (number: number) => void;
+};
+
+type MaxPartProps = {
+  value: number;
+  handler: (number: number) => void;
+};
+
+const Counter = (props: CounterProps) => {
   const { min, max, number, setNumber } = props;
   const decreaseDisabled = number <= min;
   const increaseDisabled = number >= max;
 
-  const styleContainer = {
+  const styleContainer: CSSProperties = {
     ...theme.font14,
     width: "80px",
     borderRadius: "6px",
@@ -25,7 +36,7 @@ const Counter = (props) => {
     boxShadow: theme.shadow_purple_input_inset,
   };
 
-  const styleButton = {
+  const styleButton: CSSProperties = {
     ...theme.font14_icon,
     height: "20px",
     padding: "3px",
@@ -33,7 +44,7 @@ const Counter = (props) => {
     borderRadius: "3px",
     boxShadow: theme.shadow_color_button,
   };
-  const styledDecreaseButton = {
+  const styledDecreaseButton: CSSProperties = {
     ...styleButton,
     color: decreaseDisabled ? theme.gray_text : theme.red_button,
     cursor: theme.cursor(decreaseDisabled),
@@ -41,7 +52,7 @@ const Counter = (props) => {
       ? theme.gray_background
       : theme.red_background,
   };
-  const styleIncreaseButton = {
+  const styleIncreaseButton: CSSProperties = {
     ...styleButton,
     color: increaseDisabled ? theme.gray_text : theme.green_button,
     cursor: theme.cursor(increaseDisabled),
@@ -70,16 +81,9 @@ const Counter = (props) => {
   );
 };
 
-Counter.propTypes = {
-  min: PropTypes.number,
-  max: PropTypes.number,
-  number: PropTypes.number,
-  setNumber: PropTypes.func,
-};
-
-const MaxPart = (props) => {
+const MaxPart = (props: MaxPartProps) => {
   const { value, handler } = props;
-  const styleText = {
+  const styleText: CSSProperties = {
     ...theme.font14,
     margin: "0 8px 0 6px",
     whiteSpace: "nowrap",
@@ -97,11 +101,6 @@ const MaxPart = (props) => {
       </div>
     </WhiteContainer>
   );
-};
-
-MaxPart.propTypes = {
-  value: PropTypes.number,
-  handler: PropTypes.func,
 };
 
 export default MaxPart;
