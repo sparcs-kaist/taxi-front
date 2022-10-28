a9075a042e8c897053e9532f1649da0ad5dca1d6
imYourChoi <jeehun@kaist.ac.kr>
2022-10-28 17:01:27 +0900

    Add marginTop/Bottom to DottedLine and refactor PopupRecord

---
+++ src/components/Mypage/PopupRecord/ReportList.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { theme } from "styles/theme";
+import { date2str } from "tools/moment";
+import { ReportOptionType } from "./ReportOption";
+import { ReportHistoryType } from "./index";
+import DottedLine from "components/common/DottedLine";
+import Empty from "components/common/Empty";
+
+type ReportListProps = {
+  option: ReportOptionType;
+  reportHistory: ReportHistoryType;
+};
+
+const ReportList = (props: ReportListProps) => {
+  const styleBox: CSS = {
+    display: "flex",
+    flexDirection: "column",
+    padding: "12px 12px 10px",
+    borderRadius: "12px",
+    rowGap: "6px",
+    boxShadow: theme.shadow_gray_button_inset,
+    backgroundColor: theme.gray_background,
+  };
+  const styleRow = {
+    display: "flex",
+  };
+  const styleProperty = {
+    ...theme.font12,
+    color: theme.gray_text,
+    width: "66px",
+  };
+  const styleInfo = {
+    ...theme.font12,
+  };
+  const getTypeText = (type: string) => {
+    return type === "no-settlement"
+      ? "정산 미완료"
+      : type === "no-show"
+      ? "출발 시간에 나타나지 않음"
+      : "기타 사유";
+  };
+  return props.option === "Reporting" ? (
+    !props.reportHistory?.reporting.length ? (
+      <Empty screen="mobile">신고한 기록이 없습니다</Empty>
+    ) : (
+      <>
+        {props.reportHistory?.reporting.map((report, index) => {
+          console.log(report);
+          return (
+            <div key={index} style={styleBox}>
+              <div style={styleRow}>
+                <div style={styleProperty}>신고 사유</div>
+                <div style={{ ...styleInfo, color: theme.purple }}>
+                  {getTypeText(report.type)}
+                </div>
+              </div>
+              <DottedLine direction="row" marginTop={2} marginBottom={2} />
+              <div style={styleRow}>
+                <div style={styleProperty}>별명</div>
+                <div style={styleInfo}>{report.reportedId.id}</div>
+              </div>
+              <div style={styleRow}>
+                <div style={styleProperty}>신고 일시</div>
+                <div style={styleInfo}>{date2str(report.time)}</div>
+              </div>
+            </div>
+          );
+        })}
+      </>
+    )
+  ) : !props.reportHistory?.reported.length ? (
+    <Empty screen="mobile">신고 받은 기록이 없습니다</Empty>
+  ) : (
+    <>
+      {props.reportHistory?.reported.map((report, index) => {
+        return (
+          <div key={index} style={styleBox}>
+            <div style={styleRow}>
+              <div style={styleProperty}>신고 사유</div>
+              <div style={{ ...styleInfo, color: theme.purple }}>
+                {getTypeText(report.type)}
+              </div>
+            </div>
+            <DottedLine direction="row" marginTop={2} marginBottom={2} />
+            <div style={styleRow}>
+              <div style={styleProperty}>신고 일시</div>
+              <div style={styleInfo}>{date2str(report.time)}</div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default ReportList;
---
+++ src/components/Mypage/PopupRecord/index.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { theme } from "styles/theme";
+import { date2str } from "tools/moment";
+import Modal from "components/common/modal/Modal";
+import DottedLine from "components/common/DottedLine";
+import Empty from "components/common/Empty";
+import ReportOption, { ReportOptionType } from "./ReportOption";
+import ReportList from "./ReportList";
+
+import ErrorOutlineRoundedIcon from "@mui/icons-material/ErrorOutlineRounded";
+
+export type ReportHistoryType = {
+  reported: Array<any>;
+  reporting: Array<any>;
+};
+type RecordProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  reportHistory: ReportHistoryType;
+};
+
+const PopupRecord = (props: RecordProps) => {
+  const [option, setOption] = useState<ReportOptionType>("Reporting");
+  const styleTitle = {
+    ...theme.font18,
+    display: "flex",
+    alignItems: "center",
+    marginBottom: "12px",
+  };
+  const styleGuide: CSS = {
+    ...theme.font12,
+    color: theme.gray_text,
+    wordBreak: "keep-all",
+    padding: "0 8px 12px",
+  };
+  const styleLogo = {
+    fontSize: "21px",
+    margin: "0 4px 0 8px",
+  };
+  const styleContainer: CSS = {
+    display: "flex",
+    flexDirection: "column",
+    overflow: "auto",
+    minHeight: "270px",
+    height: "calc(100vh - 360px)",
+    rowGap: "8px",
+    // maskImage:
+    //   "linear-gradient(to bottom, transparent, white 16px, white calc(100% - 16px), transparent 100%)",
+  };
+  return (
+    <Modal
+      display={props.isOpen}
+      onClickClose={props.onClose}
+      padding="16px 12px"
+    >
+      <div style={styleTitle}>
+        <ErrorOutlineRoundedIcon style={styleLogo} />
+        신고 내역
+      </div>
+      <div style={styleGuide}>
+        아래 신고 내역에 대해 문의하고 싶으신 경우 “채널톡 문의하기” 메뉴를
+        이용해주세요.
+      </div>
+      <DottedLine direction="row" />
+      <ReportOption
+        option={option}
+        onClick={(option: ReportOptionType) => setOption(option)}
+      />
+      <div style={styleContainer}>
+        <ReportList option={option} reportHistory={props.reportHistory} />
+      </div>
+    </Modal>
+  );
+};
+
+export default PopupRecord;
