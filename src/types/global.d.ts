5ea0bb24ee3604faf92428f56dcbdaa058e1f4a1
imYourChoi <jeehun@kaist.ac.kr>
2022-11-01 20:41:10 +0900

    Add: new margin and padding type globally

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,9 +2,13 @@ import { CSSProperties } from "react";
 
 export {};
 
+type Value = "0" | `${number}px`;
+
 declare global {
   type Nullable<T> = T | null | undefined;
   type CSS = CSSProperties;
+  type Margin = `${Value} ${Value}` | `${Value} ${Value} ${number}px`;
+  type Padding = `${Value} ${Value}` | `${Value} ${Value} ${number}px`;
   type ReportData = {
     reportedId: string;
     type: string;
