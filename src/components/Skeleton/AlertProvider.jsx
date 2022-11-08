bf16fc0d30b634c0ab7c344891fe3c4ca646bf99
imYourChoi <jeehun@kaist.ac.kr>
2022-11-08 15:33:32 +0900

    Modify: return null when unmounted

--- src/components/Skeleton/AlertProvider.jsx
+++ src/components/Skeleton/AlertProvider.jsx
@@ -36,51 +36,50 @@ const AlertProvider = () => {
   const onClickClose = () => setMessage(null);
   if (message) messageCache.current = message;
 
+  if (!shouldMount) return null;
   return (
-    shouldMount && (
-      <Modal
-        display={shouldDisplay}
-        onClickClose={onClickClose}
-        width={315}
-        padding="10px"
-        closeBtn={false}
-        alert
+    <Modal
+      display={shouldDisplay}
+      onClickClose={onClickClose}
+      width={315}
+      padding="10px"
+      closeBtn={false}
+      alert
+    >
+      <div
+        style={{
+          ...theme.font16_bold,
+          display: "flex",
+          alignItems: "center",
+          margin: "2px 0px 8px 6px",
+          columnGap: "4px",
+        }}
       >
-        <div
-          style={{
-            ...theme.font16_bold,
-            display: "flex",
-            alignItems: "center",
-            margin: "2px 0px 8px 6px",
-            columnGap: "4px",
-          }}
-        >
-          <WbIncandescentRoundedIcon style={{ fontSize: "16px" }} />
-          알림
-        </div>
-        <DottedLine direction="row" margin="0 2px" />
-        <div
-          style={{
-            ...theme.font14,
-            textAlign: "center",
-            wordBreak: "keep-all",
-            width: "fit-content",
-            margin: "24px auto",
-          }}
-        >
-          {messageCache.current}
-        </div>
-        <Button
-          type="purple_inset"
-          padding="9px 10px"
-          radius="8px"
-          font={theme.font14_bold}
-          onClick={onClickClose}
-        >
-          확인
-        </Button>
-      </Modal>
-    )
+        <WbIncandescentRoundedIcon style={{ fontSize: "16px" }} />
+        알림
+      </div>
+      <DottedLine direction="row" margin="0 2px" />
+      <div
+        style={{
+          ...theme.font14,
+          textAlign: "center",
+          wordBreak: "keep-all",
+          width: "fit-content",
+          margin: "24px auto",
+        }}
+      >
+        {messageCache.current}
+      </div>
+      <Button
+        type="purple_inset"
+        padding="9px 10px"
+        radius="8px"
+        font={theme.font14_bold}
+        onClick={onClickClose}
+      >
+        확인
+      </Button>
+    </Modal>
   );
 };
 
