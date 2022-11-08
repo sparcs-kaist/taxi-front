6eff4ab1ff99ae59ec50e5445ac0d410bb332592
0ev <andyye.jongcye@gmail.com>
2022-11-08 13:30:03 +0000

    Fix: change login testing

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -5,9 +5,10 @@ import "cypress-react-selector";
 const urls = ["/search", "/addroom", "/myroom", "mypage"];
 
 describe("Not authenticated user test", () => {
-  it("Block user not logged in", () => {
+  it("Block user not logged in", async () => {
     for (let url of urls) {
       cy.visit(url)
+        .wait(1000)
         .url()
         .should("not.eq", Cypress.config().baseUrl + url)
         .should("contain", "login");
