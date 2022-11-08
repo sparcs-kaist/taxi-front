41ec00c4d75265194ae3792791e240d8566de94e
0ev <andyye.jongcye@gmail.com>
2022-11-08 14:07:46 +0000

    Fix: db path

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -8,7 +8,6 @@ describe("Notgg authenticated user test", () => {
   it("Block user not logged in", async () => {
     for (let url of urls) {
       cy.visit(url)
-        .wait(1000)
         .url()
         .should("not.eq", Cypress.config().baseUrl + url)
         .should("contain", "login");
