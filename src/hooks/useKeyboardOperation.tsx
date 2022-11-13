9e6a203419b7d35237b28283ec0f6a30acbf44c8
Jinwoo Lee <starday456@kaist.ac.kr>
2022-11-13 21:42:44 +0900

    Refactor: keypress deprecated로 인해 keydown으로 통일

--- src/hooks/useKeyboardOperation.tsx
+++ src/hooks/useKeyboardOperation.tsx
@@ -21,11 +21,11 @@ const useKeyboardOperation = ({
       if (e.key === "Escape" && onEscape) onEscape();
     }
 
-    window.addEventListener("keypress", keypressEnterHandler);
+    window.addEventListener("keydown", keypressEnterHandler);
     window.addEventListener("keydown", keypressEscapeHandler);
 
     return () => {
-      window.removeEventListener("keypress", keypressEnterHandler);
+      window.removeEventListener("keydown", keypressEnterHandler);
       window.removeEventListener("keydown", keypressEscapeHandler);
     };
   }, [display, onEnter, onEscape]);
