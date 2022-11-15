ba855faf6496fe89917637eca6c0fdef2eb8ebbc
antaechan <0627antaechan@gmail.com>
2022-11-15 18:35:28 +0900

    Refactor: merge

--- src/App.js
+++ src/App.js
@@ -22,13 +22,8 @@ import "Font.css";
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <RecoilRoot>
-      <I18nextProvider i18n={i18n}>
-=======
     <CookiesProvider>
       <RecoilRoot>
->>>>>>> 43e695381897a8eae64653e2b028474698573913
         <Router>
           <ScrollRestoration />
           <ChannelTalk />
@@ -48,13 +43,8 @@ const App = () => {
             </Switch>
           </Skeleton>
         </Router>
-<<<<<<< HEAD
-      </I18nextProvider>
-    </RecoilRoot>
-=======
       </RecoilRoot>
     </CookiesProvider>
->>>>>>> 43e695381897a8eae64653e2b028474698573913
   );
 };
 
