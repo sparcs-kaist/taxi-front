f0e5f42488ddff6896c8d237df7d8b976aac1483
imYourChoi <jeehun@kaist.ac.kr>
2022-11-26 03:46:46 +0900

    Add: show room that departs or not paid/sent

--- src/components/Home/InfoSection.tsx
+++ src/components/Home/InfoSection.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import RLayout from "components/common/RLayout";
 import { useRecoilValue } from "recoil";
 import loginInfoDetailAtom from "recoil/loginInfoDetail";
 import myRoomAtom from "recoil/myRoom";
 import theme from "styles/theme";
 import moment, { getToday } from "tools/moment";
+
+import Room from "components/common/room/Room";
 import { ReactComponent as TaxiLogoWhite } from "static/assets/TaxiLogoWhite.svg";
 
 const InfoSection = () => {
@@ -32,18 +35,26 @@ const InfoSection = () => {
     const notDeparted = sortedMyRoom?.find((room) => !room.isDeparted);
     const notOver = myRoom?.ongoing.find((room) => !room.isOver);
     if (!myRoom?.ongoing.length) {
-      return "현재 참여중인 방이 없습니다.";
+      return { message: "현재 참여중인 방이 없습니다.", room: null };
     } else if (notDeparted) {
       const departure = moment(notDeparted.time);
       const diffDays = departure.diff(getToday(), "days");
       if (diffDays === 0) {
-        return "오늘 출발하는 방이 있습니다.";
+        return { message: "오늘 출발하는 방이 있습니다.", room: notDeparted };
       } else {
-        return `${diffDays}일 후 출발 예정인 방이 있습니다.`;
+        return {
+          message: `${diffDays}일 후 출발 예정인 방이 있습니다.`,
+          room: notDeparted,
+        };
       }
     } else if (notOver) {
-      return "현재 정산/결제가 완료되지 않은 방이 있습니다.\n아래 방을 눌러 완료해주세요.";
+      return {
+        message:
+          "현재 정산/결제가 완료되지 않은 방이 있습니다.\n아래 방을 눌러 완료해주세요.",
+        room: notOver,
+      };
     }
+    return { message: "", room: null };
   };
 
   return (
@@ -53,8 +64,16 @@ const InfoSection = () => {
           <TaxiLogoWhite />
           <div style={styleName}>안녕하세요, {loginInfo?.nickname}님!</div>
           <div style={{ ...theme.font14, color: theme.white }}>
-            {getMessage()}
+            {getMessage().message}
           </div>
+          {getMessage().room && (
+            <Link
+              to={`/myroom/${getMessage().room._id}`}
+              style={{ textDecoration: "none" }}
+            >
+              <Room data={getMessage().room} marginTop="24px" />
+            </Link>
+          )}
         </div>
       </RLayout.R1>
     </div>
