d143c53e195b23a0f31efd757cdd055e694db74d
Geon Kim <geon6757@kaist.ac.kr>
2023-01-17 15:22:41 +0900

    Fix: isLoading

--- src/components/Login/Logout.tsx
+++ src/components/Login/Logout.tsx
@@ -3,7 +3,7 @@ import useTaxiAPI from "hooks/useTaxiAPI";
 import { useHistory } from "react-router";
 
 const Logout = () => {
-  const [error, response, isLodding] = useTaxiAPI.get("/auth/logout");
+  const [error, response, isLoading] = useTaxiAPI.get("/auth/logout");
   const history = useHistory();
 
   useEffect(() => {
@@ -12,14 +12,14 @@ const Logout = () => {
        * @todo @fixme
        * 로그아웃 에러 처리 어떻게 할지 고민하기
        */
-    } else if (!isLodding && response) {
+    } else if (!isLoading && response) {
       /**
        * @todo @fixme
        * 글로벌 변수 초기화 필요
        */
       history.replace("/login");
     }
-  }, [error, response, isLodding]);
+  }, [error, response, isLoading]);
 
   return null;
 };
