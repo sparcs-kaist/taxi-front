88120c3500e558c9286dc5e669fb047f7240b32d
withsang <jsa5115@naver.com>
2023-02-05 20:40:39 +0900

    Add: show notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -14,5 +14,10 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log(payload);
+  const notificationTitle = payload?.data?.title || "Title does not exist";
+  const notificationOptions = {
+    body: payload?.data?.body || "Body does not exist",
+    icon: "https://sparcs-taxi-dev.s3.ap-northeast-2.amazonaws.com/profile-img/default/GooseGeoul.png",
+  };
+  self.registration.showNofication(notificationTitle, notificationOptions);
 });
