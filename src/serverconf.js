942a82cd7ffc65bdbdbf8ae701f309994f2c0d1f
14Kgun <geon6757@kaist.ac.kr>
2023-03-09 11:45:27 +0900

    Refactor: firebaseConfig

--- src/serverconf.js
+++ src/serverconf.js
@@ -10,15 +10,7 @@ const ioServer = env.REACT_APP_IO_URL ?? backServer;
 const s3BaseUrl = env.REACT_APP_S3_URL;
 const channelTalkPluginKey = env.REACT_APP_CHANNELTALK_PLUGIN_KEY;
 const gaTrackingId = env.REACT_APP_GA_TRACKING_ID;
-const firebaseConfig = {
-  apiKey: env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
+const firebaseConfig = JSON.parse(env.REACT_APP_FIREBASE_CONFIG);
 
 export {
   nodeEnv,
