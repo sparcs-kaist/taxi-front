7184845ced0045e864eda8e1287b9ca5b0f11ba1
withsang <jsa5115@naver.com>
2023-03-24 23:29:09 +0900

    Add: default title & body

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -24,10 +24,10 @@ const messaging = firebase.messaging();
 // 백그라운드 메시지가 도착했을 때 알림을 표시하는 이벤트 리스너를 추가합니다.
 messaging.onBackgroundMessage((payload) => {
   // 알림의 제목을 설정하는 notificationTitle을 생성합니다.
-  const notificationTitle = payload?.data?.title || "Title does not exist";
+  const notificationTitle = payload?.data?.title || "SPARCS Taxi";
 
   // 알림의 제목을 제외한 내용을 구성하는 notificationOption을 생성합니다.
-  const body = payload?.data?.body || "Body does not exist";
+  const body = payload?.data?.body || "There's a new message!";
   const icon = payload?.data?.icon || "/icons-512.png";
   const url = payload?.data?.url || "/";
   const notificationOptions = {
