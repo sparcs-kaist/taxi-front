f3a22f718bfec56e6e0e00e288aa8168a209aad4
14Kgun <geon6757@kaist.ac.kr>
2023-04-06 04:26:15 +0900

    Revert "Add: PopupPrivacyPolicy"
    This reverts commit 8bd373752a1b72d4fef596fdce0dbe3ec75491d9.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,12 @@
-import { useCallback } from "react";
-import { useHistory, useParams } from "react-router-dom";
-
-import PopupPrivacyPolicy from "pages/Mypage/PopupPrivacyPolicy";
-
+import { useParams } from "react-router-dom";
 import InfoSection from "./InfoSection";
 import RoomSection from "./RoomSection";
 
 const Home = () => {
-  const history = useHistory();
   const { roomId } = useParams<{ roomId: string }>();
-
-  const onCloseModal = useCallback(() => history.replace("/home"), [history]);
 
   return (
     <>
-      <PopupPrivacyPolicy
-        isOpen={roomId === "privacyPolicy"}
-        onClose={onCloseModal}
-      />
       <InfoSection />
       <RoomSection roomId={roomId} />
     </>
