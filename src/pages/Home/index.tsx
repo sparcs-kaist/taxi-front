8bd373752a1b72d4fef596fdce0dbe3ec75491d9
14Kgun <geon6757@kaist.ac.kr>
2023-04-06 04:18:02 +0900

    Add: PopupPrivacyPolicy

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,23 @@
-import { useParams } from "react-router-dom";
+import { useCallback } from "react";
+import { useHistory, useParams } from "react-router-dom";
+
+import PopupPrivacyPolicy from "pages/Mypage/PopupPrivacyPolicy";
+
 import InfoSection from "./InfoSection";
 import RoomSection from "./RoomSection";
 
 const Home = () => {
+  const history = useHistory();
   const { roomId } = useParams<{ roomId: string }>();
+
+  const onCloseModal = useCallback(() => history.replace("/home"), [history]);
 
   return (
     <>
+      <PopupPrivacyPolicy
+        isOpen={roomId === "privacyPolicy"}
+        onClose={onCloseModal}
+      />
       <InfoSection />
       <RoomSection roomId={roomId} />
     </>
