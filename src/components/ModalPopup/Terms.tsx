55dd6cfdeec1ee18164c080bd9162ca7ea167880
14Kgun <geon6757@kaist.ac.kr>
2023-04-10 17:33:06 +0900

    Fix: Terms

--- src/components/ModalPopup/Terms.tsx
+++ src/components/ModalPopup/Terms.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import theme from "tools/theme";
 
 const Terms = () => {
@@ -129,17 +131,40 @@ const Terms = () => {
         3. 본 약관의 해석은 택시 운영진이 담당하며, 분쟁이 있을 경우 민법 등
         관계 법률과 관례에 따릅니다.
       </div>
-      <div style={styleSection}>개인정보취급방침</div>
+      <div style={styleSection}>제 7조. 개인정보 수집 및 이용</div>
       <div style={styleContent}>
         1. SPARCS는 개인정보 보호법 제30조에 따라 택시를 이용하는 사용자의
         개인정보를 보호하고 이와 관련한 고충을 신속하고 원활하게 처리할 수
-        있도록 하기 위하여&nbsp;
-        <a href="https://sparcssso.kaist.ac.kr/privacy/">
-          SPARCS SSO의 개인정보취급방침
-        </a>
-        &nbsp;을 따릅니다.
+        있도록 하기 위하여{" "}
+        <Link
+          to={(location) => ({
+            ...location,
+            pathname: location.pathname.startsWith("/mypage")
+              ? "/mypage/privacyPolicy"
+              : "/home/privacyPolicy",
+          })}
+          replace
+        >
+          개인정보 취급방침
+        </Link>{" "}
+        을 따릅니다.
+        <br />
+        2. SPARCS는 개인정보보호법 제 15조에 따라 회원의 동의를 받아 회원가입 및
+        이용 시 아래와 같이 개인정보가 수집 및 이용되어짐을 안내드립니다.
+        <br />- 개인정보의 수집 및 이용 목적 : 홈페이지 회원가입 및 관리
+        <br />- 수집 및 이용되는 개인정보의 항목 : 이메일, 이름, 접속 로그, 접속
+        IP 정보, 학번
+        <br />- 개인정보의 보유 및 이용 기간 : 탈퇴일로부터 1년
+        <br />
+        3. 개인정보 수집 및 이용을 거부하실 수 있으나, 이는 서비스 제공에
+        필수적으로 제공되어지는 정보이므로, 동의를 거부하실 경우 택시 서비스를
+        이용 하실 수 없습니다.
       </div>
-      <div style={styleSection}>본 약관은 2022-08-20부터 적용됩니다.</div>
+      <div style={styleSection}>본 약관은 2023-04-10부터 적용됩니다.</div>
+      <div style={styleSection}>
+        본인은 위 내용을 충분히 숙지하였으며, 이용 약관, 개인정보 수집 및 이용에
+        동의합니다.
+      </div>
     </div>
   );
 };
