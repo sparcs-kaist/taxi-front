4d224d5bacd9d723b6f10ab0488869ba8b42b2c3
14Kgun <geon6757@kaist.ac.kr>
2023-04-20 01:31:08 +0900

    Refactor: Search

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,5 +1,5 @@
 import qs from "qs";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useCookies } from "react-cookie";
 import { useHistory, useLocation } from "react-router-dom";
 
@@ -21,91 +21,180 @@ import Tooltip from "components/Tooltip";
 
 import SelectSearchOptions from "./SelectSearchOptions";
 import SideResult from "./SideResult";
-import { isSearchAll, isValidQuery } from "./utils";
+import { isValidQuery } from "./utils";
+
+import errorAtom from "atoms/error";
+import { useSetRecoilState } from "recoil";
 
 import moment, { getToday, getToday10 } from "tools/moment";
 import theme from "tools/theme";
 
-const searchQueryOption = { skipNulls: true };
-const defaultOptions = { place: true, date: true, time: true };
-
 const Search = () => {
-  const reactiveState = useR2state();
-  const onCall = useRef(false);
-  const prevSearchParam = useRef("");
-  const scrollRef = useRef(null);
-  const today = useRef(getToday());
-  const today10 = getToday10();
-  const history = useHistory();
-  const location = useLocation();
   const axios = useAxios();
-
   const [cookies, setCookie] = useCookies(["defaultFromTo"]);
+  const history = useHistory();
+  const { search } = useLocation();
+  const reactiveState = useR2state();
+  const today10 = getToday10();
+  const setError = useSetRecoilState(errorAtom);
+
+  const defaultOptions = {
+    place: true,
+    date: true,
+    time: true,
+    maxPeople: false,
+    name: false,
+  };
+  const defaultPlace = [
+    cookies?.defaultFromTo?.[0] ?? null,
+    cookies?.defaultFromTo?.[1] ?? null,
+  ];
+  const defaultDate = [today10.year(), today10.month() + 1, today10.date()];
+  const defaultTime = [today10.hour(), today10.minute()];
+  const defaultMaxPeople = 4;
+  const defaultName = "";
+
   const [searchOptions, setSearchOptions] = useState(defaultOptions);
-  const [valueName, setName] = useState("");
-  const defaultPlace =
-    cookies?.defaultFromTo?.[0] && cookies?.defaultFromTo?.[1]
-      ? cookies.defaultFromTo
-      : [null, null];
   const [valuePlace, setPlace] = useState(defaultPlace);
-  const [valueDate, setDate] = useState([null, null, null]);
-  const [valueTime, setTime] = useState([today10.hour(), today10.minute()]);
-  const [valueMaxPeople, setMaxPeople] = useState(null);
+  const [valueDate, setDate] = useState(defaultDate);
+  const [valueTime, setTime] = useState(defaultTime);
+  const [valueMaxPeople, setMaxPeople] = useState(defaultMaxPeople);
+  const [valueName, setName] = useState(defaultName);
+
   const [searchResult, setSearchResult] = useState(null);
-  const [disabled, setDisabled] = useState(true);
-  const [message, setMessage] = useState("검색 조건을 선택해주세요");
+
+  const scrollRef = useRef(null);
   const [showScrollButton, setShowScrollButton] = useState(false);
 
   useEffect(() => {
-    const expirationDate = new Date();
-    expirationDate.setFullYear(expirationDate.getFullYear() + 10);
     if (valuePlace[0] && valuePlace[1]) {
+      const expirationDate = new Date();
+      expirationDate.setFullYear(expirationDate.getFullYear() + 10);
       setCookie("defaultFromTo", valuePlace, {
         expires: expirationDate,
       });
     }
   }, [valuePlace]);
 
-  const clearState = () => {
-    onCall.current = false;
-    setSearchOptions(defaultOptions);
-    setName("");
-    setPlace(defaultPlace);
-    setDate([
-      today.current.year(),
-      today.current.month() + 1,
-      today.current.date(),
-    ]);
-    setTime([today10.hour(), today10.minute()]);
-    setMaxPeople(null);
-    setSearchResult(null);
-    setDisabled(true);
-    setMessage("검색 조건을 선택해주세요");
-  };
-
-  const setStatesFromQuery = (q) => {
-    const newSearchOptions = { ...searchOptions };
-    const entries = Object.entries(q);
-    for (let [key, val] of entries) {
-      if (key === "name" && val !== null) newSearchOptions.name = true;
-      if (key === "from" && val !== null) newSearchOptions.place = true;
-      if (key === "time" && val !== null) newSearchOptions.date = true;
-      if (key === "withTime" && val === "true") newSearchOptions.time = true;
-      if (key === "maxPeople" && val !== null)
+  // search query에 따라 검색 결과 설정
+  useEffect(() => {
+    try {
+      const query = qs.parse(search.slice(1));
+      if (Object.keys(query).length === 0) {
+        setSearchResult(null);
+        return;
+      }
+      if (!isValidQuery(query)) {
+        throw Error("Parameter is not valid");
+      }
+      const newSearchOptions = {
+        place: false,
+        date: false,
+        time: false,
+        maxPeople: false,
+        name: false,
+      };
+      if (query.place) {
+        newSearchOptions.place = true;
+        setPlace([query.place[0], query.place[1]]);
+      }
+      if (query.date) {
+        newSearchOptions.date = true;
+        setDate(query.date.map((x) => parseInt(x)));
+      }
+      if (query.time) {
+        newSearchOptions.time = true;
+        setTime(query.time.map((x) => parseInt(x)));
+      }
+      if (query.maxPeople) {
         newSearchOptions.maxPeople = true;
-    }
-    setSearchOptions(newSearchOptions);
-    if (newSearchOptions.name) setName(q.name);
-    if (newSearchOptions.place) setPlace([q.from, q.to]);
-    if (newSearchOptions.date) {
-      const queryTime = moment(q.time);
-      setDate([queryTime.year(), queryTime.month() + 1, queryTime.date()]);
-      if (newSearchOptions.time)
-        setTime([queryTime.hour(), Math.floor(queryTime.minute() / 10) * 10]);
-    }
-    if (newSearchOptions.maxPeople) setMaxPeople(Number(q.maxPeople));
-  };
+        setMaxPeople(parseInt(query.maxPeople));
+      }
+      if (query.name) {
+        newSearchOptions.name = true;
+        setName(query.name);
+      }
+      setSearchOptions(newSearchOptions);
 
+      let time = null;
+      let withTime = false;
+      if (query.date) {
+        time = moment();
+        time.year(query.date[0]);
+        time.month(query.date[1] - 1);
+        time.date(query.date[2]);
+      }
+      if (query.time) {
+        time.hour(query.time[0]);
+        time.minute(query.time[1]);
+        withTime = true;
+      }
+      const params = {
+        name: query.name,
+        from: query.place?.[0],
+        to: query.place?.[1],
+        time: time?.toISOString(),
+        withTime,
+        maxPartLength: query.maxPeople,
+      };
+      axios({
+        url: "/rooms/search",
+        method: "get",
+        params,
+        onSuccess: (data) => setSearchResult(data),
+      });
+    } catch (e) {
+      setError({
+        title: "페이지를 찾을 수 없습니다.",
+        message: "올바르지 않은 방 검색 조건입니다.",
+        record: null,
+      });
+    }
+  }, [search]);
+
+  // 검색 버튼 메시지 설정
+  const [message, isDisabled] = useMemo(() => {
+    if (!Object.values(searchOptions).some((option) => option)) {
+      return ["빠른 출발 검색", false];
+    } else if (searchOptions.name && valueName === "") {
+      return ["방 이름을 입력해주세요", true];
+    } else if (
+      (searchOptions.place && valuePlace.some((place) => place === null)) ||
+      (searchOptions.date && valueDate.some((date) => date === null)) ||
+      (searchOptions.time && valueTime.some((time) => time === null))
+    ) {
+      return ["선택을 완료해주세요", true];
+    } else if (
+      searchOptions.place &&
+      valuePlace[0] !== null &&
+      valuePlace[1] !== null &&
+      valuePlace[0] === valuePlace[1]
+    ) {
+      return ["출발지와 도착지는 달라야 합니다", true];
+    } else if (
+      searchOptions.time &
+      !valueDate.some((date) => date === null) &
+      moment(
+        `${valueDate[0]}-${
+          valueDate[1] < 10 ? "0" + valueDate[1] : valueDate[1]
+        }-${valueDate[2]} ${valueTime[0]}:${valueTime[1]}`
+      ).isBefore(getToday(), "minute")
+    ) {
+      return ["과거 시점은 검색할 수 없습니다", true];
+    }
+    return ["방 검색하기", false];
+  }, [searchOptions, valueName, valuePlace, valueDate, valueTime]);
+
+  // 검색 결과 생성 후 스크롤
+  useEffect(() => {
+    if (!searchResult || reactiveState !== 3) return;
+    setTimeout(() => {
+      const scrollToResult = scrollRef.current?.offsetTop + 79; // '검색 결과'의 parent 내에서의 offset + '방 검색하기'의 height
+      window.scrollTo({ top: scrollToResult, behavior: "smooth" });
+    }, 0);
+  }, [searchResult]);
+
+  // scroll & resize 이벤트 등록 및 해제
   useEffect(() => {
     const onScrollOrResize = () => {
       if (!searchResult && reactiveState !== 3) return;
@@ -121,159 +210,18 @@ const Search = () => {
     };
   }, []);
 
-  useEffect(() => {
-    const q = qs.parse(location.search.slice(1), searchQueryOption);
-
-    if (Object.keys(q).length === 0) {
-      clearState();
-      return;
-    }
-
-    const searchParamWithoutPage = qs.stringify({ ...q, page: null });
-    if (prevSearchParam.current === searchParamWithoutPage) return;
-    prevSearchParam.current = searchParamWithoutPage;
-
-    if (isSearchAll(q)) {
-      axios({
-        url: "rooms/search",
-        method: "get",
-        onSuccess: (data) => setSearchResult(data),
-      });
-    } else if (isValidQuery(q)) {
-      setStatesFromQuery(q);
-      if (q.maxPeople) {
-        delete Object.assign(q, { maxPartLength: q.maxPeople }).maxPeople;
-      }
-      axios({
-        url: "rooms/search",
-        method: "get",
-        params: q,
-        onSuccess: (data) => setSearchResult(data),
-      });
-    } else {
-      history.replace("/search");
-    }
-  }, [location.search]);
-
-  useEffect(() => {
-    if (!Object.values(searchOptions).some((option) => option)) {
-      setMessage("빠른 출발 검색");
-      setDisabled(false);
-    } else if (searchOptions.name && valueName === "") {
-      setMessage("방 이름을 입력해주세요");
-      setDisabled(true);
-    } else if (
-      (searchOptions.place && valuePlace.some((place) => place === null)) ||
-      (searchOptions.date && valueDate.some((date) => date === null)) ||
-      (searchOptions.time && valueTime.some((time) => time === null))
-    ) {
-      setMessage("선택을 완료해주세요");
-      setDisabled(true);
-    } else if (
-      (valuePlace[0] !== null || valuePlace[1] !== null) &&
-      valuePlace[0] === valuePlace[1]
-    ) {
-      setMessage("출발지와 도착지는 달라야 합니다");
-      setDisabled(true);
-    } else if (
-      searchOptions.time &
-      !valueDate.some((date) => date === null) &
-      moment(
-        `${valueDate[0]}-${
-          valueDate[1] < 10 ? "0" + valueDate[1] : valueDate[1]
-        }-${valueDate[2]} ${valueTime[0]}:${valueTime[1]}`
-      ).isBefore(getToday(), "minute")
-    ) {
-      setMessage("과거 시점은 검색할 수 없습니다.");
-      setDisabled(true);
-    } else {
-      setMessage("방 검색하기");
-      setDisabled(false);
-    }
-  }, [searchOptions, valueName, valuePlace, valueDate, valueTime]);
-
-  useEffect(() => {
-    if (!searchOptions.name && valueName.length > 0) setName("");
-  }, [searchOptions.name]);
-  useEffect(() => {
-    if (
-      !searchOptions.place &&
-      (valuePlace[0] !== null || valuePlace[1] !== null)
-    )
-      setPlace(defaultPlace);
-  }, [searchOptions.place]);
-  useEffect(() => {
-    if (!searchOptions.date && valueDate[0] !== null)
-      setDate([null, null, null]);
-    if (searchOptions.date) {
-      today.current = getToday();
-      setDate([
-        today.current.year(),
-        today.current.month() + 1,
-        today.current.date(),
-      ]);
-    }
-  }, [searchOptions.date]);
-  useEffect(() => {
-    if (searchOptions.time) {
-      if (valueTime[0] === 0 && valueTime[1] === 0) {
-        const today = getToday10();
-        setTime([today.hour(), today.minute()]);
-      }
-    } else if (valueTime[0] !== 0 || valueTime[1] !== 0) {
-      setTime([today10.hour(), today10.minute()]);
-    }
-  }, [searchOptions.time]);
-  useEffect(() => {
-    if (searchOptions.maxPeople) {
-      if (valueMaxPeople === null) setMaxPeople(4);
-    } else if (valueMaxPeople !== null) setMaxPeople(null);
-  }, [searchOptions.maxPeople]);
-
-  const onClickSearch = async () => {
-    if (!onCall.current) {
-      onCall.current = true;
-      setSearchResult([]);
-    }
-
-    if (!Object.values(searchOptions).some((option) => option)) {
-      history.push("/search?all=true");
-    } else {
-      let withTime = false;
-      let date = null;
-      if (searchOptions.date && valueDate[0] !== null) {
-        date = moment();
-        date.year(valueDate[0]);
-        date.month(valueDate[1] - 1);
-        date.date(valueDate[2]);
-      }
-      if (searchOptions.time) {
-        date.hour(valueTime[0]);
-        date.minute(valueTime[1]);
-        withTime = true;
-      }
-      const q = qs.stringify(
-        {
-          name: valueName.length ? valueName : null,
-          from: valuePlace[0],
-          to: valuePlace[1],
-          time: date?.toISOString(),
-          withTime,
-          maxPeople: valueMaxPeople,
-        },
-        searchQueryOption
-      );
-      history.push(`/search?${q}`);
-    }
+  // 검색 버튼 클릭 시
+  const onClickSearch = () => {
+    const query = qs.stringify({
+      place: searchOptions.place ? valuePlace : undefined,
+      date: searchOptions.date ? valueDate : undefined,
+      time: searchOptions.time ? valueTime : undefined,
+      maxPeople: searchOptions.maxPeople ? valueMaxPeople : undefined,
+      name: searchOptions.name ? valueName : undefined,
+      page: 1,
+    });
+    history.push(`/search?${query}`);
   };
-
-  useEffect(() => {
-    if (!onCall.current || reactiveState !== 3) return;
-    setTimeout(() => {
-      const scrollToResult = scrollRef.current?.offsetTop + 79; // '검색 결과'의 parent 내에서의 offset + '방 검색하기'의 height
-      window.scrollTo({ top: scrollToResult, behavior: "smooth" });
-    }, 0);
-  }, [searchResult]);
 
   const leftLay = (
     <>
@@ -299,7 +247,7 @@ const Search = () => {
       {searchOptions.name && <OptionName value={valueName} handler={setName} />}
       <Button
         type="purple"
-        disabled={disabled}
+        disabled={isDisabled}
         padding="14px 0 13px"
         radius={12}
         font={theme.font16_bold}
