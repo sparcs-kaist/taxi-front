3f855c685f42d329005a5385417b61e42c2fdddc
14Kgun <geon6757@kaist.ac.kr>
2023-04-27 17:35:30 +0900

    Add: deviceToken and accessToken to loginInfo atom

--- src/components/Link/LinkLogin.tsx
+++ src/components/Link/LinkLogin.tsx
@@ -1,5 +1,7 @@
 import { useLocation } from "react-router-dom";
 
+import { isWebViewInFlutter } from "tools/sendEventToFlutter";
+
 import { backServer } from "loadenv";
 
 type LinkLoginProps = {
@@ -10,11 +12,13 @@ type LinkLoginProps = {
 const LinkLogin = ({ children, redirect }: LinkLoginProps) => {
   const { pathname, search } = useLocation();
   const redirectPath = redirect || pathname + search;
+  const isApp = isWebViewInFlutter();
+
   return (
     <a
       href={`${backServer}/auth/sparcssso?redirect=${encodeURIComponent(
         redirectPath
-      )}`}
+      )}${isApp ? "&isApp=true" : ""}`}
       css={{ textDecoration: "none" }}
     >
       {children}
