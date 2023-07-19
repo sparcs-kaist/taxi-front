459af6a7fb180622e67f1f06d7aae554f0fa5c65
cokia <hanu@hanukoon.com>
2023-07-19 20:54:34 +0900

    chore: use style at custom.d.ts

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,26 +1,26 @@
-declare module '*.svg' {
-  import React = require('react');
+declare module "*.svg" {
+  import React = require("react");
   export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
   const src: string;
   export default src;
 }
 
-declare module '*.png' {
-  import React = require('react');
+declare module "*.png" {
+  import React = require("react");
   export const ReactComponent: React.FC<any>;
   const src: string;
   export default src;
 }
 
-declare module '*.jpg' {
-  import React = require('react');
+declare module "*.jpg" {
+  import React = require("react");
   export const ReactComponent: React.FC<any>;
   const src: string;
   export default src;
 }
 
-declare module '*.webp' {
-  import React = require('react');
+declare module "*.webp" {
+  import React = require("react");
   export const ReactComponent: React.FC<any>;
   const src: string;
   export default src;
