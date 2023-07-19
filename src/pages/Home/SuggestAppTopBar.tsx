9971f610e7deab519ad5d587060a8861bb7942a1
0ev <andyye.jongcye@gmail.com>
2023-07-19 19:26:32 +0900

    Add: logic and erase modal

--- src/pages/Home/SuggestAppTopBar.tsx
+++ src/pages/Home/SuggestAppTopBar.tsx
@@ -1,8 +1,14 @@
+import { useLocation } from "react-router-dom";
+
 import theme from "tools/theme";
+import { getDynamicLink } from "tools/trans";
 
 import { ReactComponent as TaxiIcon } from "static/assets/TaxiAppIcon.svg";
 
 const SuggestAppTopBar = () => {
+  const { pathname, search } = useLocation();
+  const currentPath = pathname + search;
+
   return (
     <div
       css={{
@@ -10,14 +16,21 @@ const SuggestAppTopBar = () => {
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-between",
-        width: "100%",
+        // width: "100%",
         height: "50px",
         backgroundColor: theme.purple,
 
         padding: "0 10px",
       }}
     >
-      <div css={{ display: "flex" }}>
+      <div
+        css={{
+          display: "flex",
+          gap: "10px",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <TaxiIcon css={{ width: "30px", height: "30px" }} />
         <div>
           <div css={{ ...theme.font14_bold, color: theme.white }}>
@@ -28,7 +41,21 @@ const SuggestAppTopBar = () => {
           </div>
         </div>
       </div>
-      <div>열기</div>
+      <a
+        href={getDynamicLink(currentPath, false)}
+        target="_blank"
+        rel="noreferrer"
+        css={{
+          ...theme.font14_bold,
+          borderRadius: "15px",
+          padding: "7px 20px",
+          backgroundColor: theme.white,
+          boxShadow: theme.shadow_3_up,
+          textDecoration: "none",
+        }}
+      >
+        열기
+      </a>
     </div>
   );
 };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { useHistory, useParams } from "react-router-dom";
 
+import { useValueRecoilState } from "hooks/useFetchRecoilState";
+
 import Footer from "components/Footer";
 import { ModalPrivacyPolicy } from "components/ModalPopup";
 
@@ -7,16 +9,25 @@ import InfoSection from "./InfoSection";
 import RoomSection from "./RoomSection";
 import SuggestAppTopBar from "./SuggestAppTopBar";
 
+import isAppAtom from "atoms/isApp";
+import { useRecoilValue } from "recoil";
+
+import isMobile from "tools/isMobile";
+
 const Home = () => {
   const history = useHistory();
   const { roomId: _roomId } = useParams<{ roomId: string }>();
   const roomId = _roomId === "privacyPolicy" ? null : _roomId;
 
+  const { deviceType } = useValueRecoilState("loginInfo") || {};
+  const isApp = useRecoilValue(isAppAtom) || deviceType === "app";
+  const [isAndroid, isIOS] = isMobile();
+
   const onChangeIsOpenPrivacyPolicy = () => history.replace("/home");
 
   return (
     <>
-      <SuggestAppTopBar />
+      {(isAndroid || isIOS) && !isApp ? <SuggestAppTopBar /> : null}
       <InfoSection />
       <div css={{ marginTop: "-10px" }} />
       <RoomSection roomId={roomId} />
