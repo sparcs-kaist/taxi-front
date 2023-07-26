173ef27d264cba6381bd00f53f43e7d8e6db0431
0ev <andyye.jongcye@gmail.com>
2023-07-26 22:37:04 +0900

    Fix: stupid conflict resolve

---
+++ src/pages/Home/SuggestAppSection.tsx
@@ -0,0 +1,14 @@
+import AdaptiveDiv from "components/AdaptiveDiv";
+import Title from "components/Title";
+
+const SuggestAppSection = () => {
+  return (
+    <AdaptiveDiv type="center">
+      <Title icon="taxi" isHeader>
+        이벤트
+      </Title>
+    </AdaptiveDiv>
+  );
+};
+
+export default SuggestAppSection;
