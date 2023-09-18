e5989c667ed6b136d4a240a21fd17ebf5f3e5b90
14Kgun <geon6757@kaist.ac.kr>
2023-09-18 20:59:14 +0900

    Refactor: ModalEvent2023FallRandomBox

--- src/pages/Event/Event2023Fall.tsx
+++ src/pages/Event/Event2023Fall.tsx
@@ -1,15 +1,23 @@
 import { memo } from "react";
 
 import AdaptiveDiv from "components/AdaptiveDiv";
+import HeaderWithBackButton from "components/Header/HeaderWithBackButton";
 import Title from "components/Title";
+
+import theme from "tools/theme";
 
 const Event2023Fall = () => {
   return (
-    <AdaptiveDiv type="center">
-      <Title icon="notice" isHeader>
-        2023Fall 이벤트 - TODO
-      </Title>
-    </AdaptiveDiv>
+    <>
+      <HeaderWithBackButton>
+        <div css={{ color: theme.purple, ...theme.font18 }}>이벤트 안내</div>
+      </HeaderWithBackButton>
+      <AdaptiveDiv type="center">
+        <Title icon="notice" isHeader>
+          2023Fall 이벤트 - TODO
+        </Title>
+      </AdaptiveDiv>
+    </>
   );
 };
 
