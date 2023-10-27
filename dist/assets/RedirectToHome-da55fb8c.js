20b9ef47f36b37d64f8f4a259e4da1ea4b2cdda2
andyye <63383967+0ev@users.noreply.github.com>
2023-10-27 21:14:29 +0900

    Fix: change build directory

--- dist/assets/RedirectToHome-da55fb8c.js
+++
@@ -1,1 +0,0 @@
-import{b as e,f as o}from"./index-ffb5f151.js";const r=()=>e(o,{to:"/home"});export{r as default};
--- dist/assets/index-ac7fbdbb.js
+++
@@ -1,1 +0,0 @@
-import{y as t,b as s,bf as o}from"./index-ffb5f151.js";const e=()=>{const{roomId:a}=t();return s(o,{roomId:a,layoutType:"fullchat"})};export{e as default};
