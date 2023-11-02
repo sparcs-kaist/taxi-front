152f7c771be4ca0b3a5975e61219bc423c727b3b
andyye <63383967+0ev@users.noreply.github.com>
2023-11-03 00:27:07 +0900

    Revert: reverted changes to index.html

--- vite.config.ts
+++ vite.config.ts
@@ -12,11 +12,6 @@ export default defineConfig({
   },
   build: {
     outDir: "./build",
-    rollupOptions: {
-      input: {
-        app: "./public/index.html",
-      },
-    },
   },
   server: {
     port: 3000,
