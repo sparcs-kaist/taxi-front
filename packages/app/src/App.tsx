78f77f960bf03f365fd86fed42cf0d47dd8f1862
happycastle <41810556+happycastle114@users.noreply.github.com>
2024-01-30 21:15:27 +0900

    Fix: SafeArea At Android

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,15 +1,23 @@
 import { RootStack } from "@/screens";
 import { NavigationContainer } from "@react-navigation/native";
+import Constants from "expo-constants";
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import { Platform, SafeAreaView, View } from "react-native";
 
 export function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <NavigationContainer>
-        <RootStack />
-      </NavigationContainer>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
+        }}
+      >
+        <NavigationContainer>
+          <RootStack />
+        </NavigationContainer>
+      </SafeAreaView>
+    </>
   );
 }
