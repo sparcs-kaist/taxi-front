cc0e067dcaefa04b5b8778bd160d3786d4652f29
happycastle <41810556+happycastle114@users.noreply.github.com>
2024-01-31 00:13:30 +0900

    Fix: Vite .env At config

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,31 +1,34 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig({
-  plugins: [svgr(), react({ jsxImportSource: "@emotion/react" })],
-  envDir: "../..",
-  envPrefix: "REACT_APP_",
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-  build: {
-    outDir: "./build",
-  },
-  server: {
-    port: 3000,
-    host: true,
-    proxy: {
-      "/api": {
-        target: process.env.REACT_APP_BACK_URL ?? "http://localhost:9000",
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-      "/socket.io": {
-        target: process.env.REACT_APP_IO_URL ?? "http://localhost:9000",
-        ws: true,
+export default defineConfig((command, mode) => {
+  const env = loadEnv(mode, "../..", "REACT_APP_");
+  return {
+    plugins: [svgr(), react({ jsxImportSource: "@emotion/react" })],
+    envDir: "../..",
+    envPrefix: "REACT_APP_",
+    resolve: {
+      alias: {
+        "@": "/src",
       },
     },
-  },
+    build: {
+      outDir: "./build",
+    },
+    server: {
+      port: 3000,
+      host: true,
+      proxy: {
+        "/api": {
+          target: env.REACT_APP_BACK_URL ?? "http://localhost:9000",
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+        "/socket.io": {
+          target: env.REACT_APP_IO_URL ?? "http://localhost:9000",
+          ws: true,
+        },
+      },
+    },
+  };
 });
