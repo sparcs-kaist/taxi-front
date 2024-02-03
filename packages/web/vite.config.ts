314aa1750b8e1abbca5ebdcf888b5c4a2a9d0565
happycastle <41810556+happycastle114@users.noreply.github.com>
2024-02-03 15:08:49 +0900

    Fix: Vite config for env

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig, loadEnv } from "vite";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig((command, mode) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, "../..", "REACT_APP_");
   return {
     plugins: [svgr(), react({ jsxImportSource: "@emotion/react" })],
