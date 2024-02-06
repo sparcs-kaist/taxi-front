63d3dbaedcf77af5fd8c8934d8694ac0013afc62
Soonho Kwon <percy3368@gmail.com>
2024-02-06 17:49:17 +0900

    Add: Init `@taxi/core` package

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,9 +1,11 @@
 import { RootStack } from "@/screens";
 import { NavigationContainer } from "@react-navigation/native";
+import { hello } from "@taxi/core";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 
 export function App() {
+  hello(); // test
   return (
     <View style={{ flex: 1 }}>
       <NavigationContainer>
