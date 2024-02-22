6a67ffede5521a286549847c500faaccd6a31b78
andyye <63383967+0ev@users.noreply.github.com>
2024-02-23 06:28:31 +0900

    Add:dday

--- packages/web/src/tools/day.ts
+++ packages/web/src/tools/day.ts
@@ -41,6 +41,14 @@ export const day2str = (day: Dayjs, format = "LLLL") => {
   return day.format(format);
 };
 
+export const getDday = (dateString: string) => {
+  const targetDate = dayjs(dateString);
+  const currentDate = dayNowServer(); // Get current server time
+  const diffDays = currentDate.diff(targetDate, "day");
+
+  return `D-${Math.abs(diffDays).toString().padStart(3, "0")}`;
+};
+
 /** 다음은 초, 분, 시간, 일, 월, 년을 고려하여 시간 차이를 문자열로 반환 */
 export const dayDifference2str = (date1: Dayjs, date2: Dayjs) => {
   const diffInMilliseconds = date1.diff(date2);
