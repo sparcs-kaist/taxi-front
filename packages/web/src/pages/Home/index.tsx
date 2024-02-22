38a337e068e5926883bc2ac3b51151664c8b67e4
andyye <63383967+0ev@users.noreply.github.com>
2024-02-23 03:49:28 +0900

    Add referer

--- packages/web/src/pages/Home/index.tsx
+++ packages/web/src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
+import { useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
+
+import useQuery from "@/hooks/useTaxiAPI";
 
 import Footer from "@/components/Footer";
 import { ModalPrivacyPolicy } from "@/components/ModalPopup";
@@ -7,12 +10,30 @@ import EventSection from "./EventSection";
 import InfoSection from "./InfoSection";
 import RoomSection from "./RoomSection";
 
+import { getDynamicLink } from "@/tools/trans";
+
 const Home = () => {
   const history = useHistory();
-  const { roomId: _roomId } = useParams<{ roomId: string }>();
+  const { roomId: _roomId, eventStatusId: eventStatusId } = useParams<{
+    roomId: string;
+    eventStatusId: string;
+  }>();
   const roomId = _roomId === "privacyPolicy" ? null : _roomId;
 
+  const [, eventProfile] = useQuery.get(
+    `/events/2024spring/invite/search/:${roomId}`,
+    {},
+    [eventStatusId]
+  );
+
   const onChangeIsOpenPrivacyPolicy = () => history.replace("/home");
+
+  useEffect(() => {
+    if (!eventStatusId) return;
+    const dynamicLink = getDynamicLink(window.location.pathname);
+    window.location.replace(dynamicLink);
+    
+  }, [eventStatusId]);
 
   return (
     <>
@@ -25,6 +46,7 @@ const Home = () => {
         isOpen={_roomId === "privacyPolicy"}
         onChangeIsOpen={onChangeIsOpenPrivacyPolicy}
       />
+      {/* 모달 완성되면 넣을 곳 */}
     </>
   );
 };
