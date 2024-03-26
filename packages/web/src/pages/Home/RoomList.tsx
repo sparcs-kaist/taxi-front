e60e8f434f7b8175f0f402856f2e86a835334149
andyye <63383967+0ev@users.noreply.github.com>
2024-03-26 20:45:46 +0900

    Add: room diff calculation

--- packages/web/src/pages/Home/RoomList.tsx
+++ packages/web/src/pages/Home/RoomList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import usePageFromSearchParams from "@/hooks/usePageFromSearchParams";
@@ -13,6 +14,43 @@ type RoomListProps = {
 const RoomList = (props: RoomListProps) => {
   const totalPages = Math.ceil((props.rooms ?? []).length / PAGE_MAX_ITEMS);
   const currentPage = usePageFromSearchParams(totalPages);
+  const [localRooms, setLocalRooms] = useState(props.rooms);
+
+  // calculates the difference between two arrays location of the difference. Only works when before is subset of after
+  const calculateDiffAndLocation = (before: Array<any>, after: Array<any>) => {
+    let beforeIndex = 0;
+    let afterIndex = 0;
+    let diffs: {
+      diff: any;
+      location: number;
+    }[] = [];
+    while (beforeIndex < before.length && afterIndex < after.length) {
+      if (before[beforeIndex] !== after[afterIndex]) {
+        diffs.push({
+          diff: after[afterIndex],
+          location: afterIndex,
+        });
+      } else {
+        beforeIndex++;
+      }
+      afterIndex++;
+    }
+    if (beforeIndex < before.length) {
+      return null;
+    }
+    return diffs;
+  };
+
+  useEffect(() => {
+    if (props.rooms && localRooms && props.rooms.length !== localRooms.length) {
+      const diffs = calculateDiffAndLocation(localRooms, props.rooms);
+      if (diffs) {
+        setLocalRooms(props.rooms);
+      }
+    } else {
+      setLocalRooms(props.rooms);
+    }
+  }, [props.rooms]);
 
   return (
     <>
