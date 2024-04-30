5e51b77ee3cc9a7301f112e1f22b83168d67873f
andyye <63383967+0ev@users.noreply.github.com>
2024-04-30 20:18:40 +0900

    Add: Convert animation to hook and Add AnimatedRoom component to Myroom and Home pages

--- packages/web/src/pages/Home/RoomList.tsx
+++ packages/web/src/pages/Home/RoomList.tsx
@@ -1,13 +1,11 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import usePageFromSearchParams from "@/hooks/usePageFromSearchParams";
+import useRoomListAnimationState from "@/hooks/useRoomListAnimationState";
 
 import Empty from "@/components/Empty";
 import Pagination, { PAGE_MAX_ITEMS } from "@/components/Pagination";
-import Room from "@/components/Room";
-
-import NewRoom from "./NewRoom";
+import AnimatedRoom from "@/components/Room/AnimatedRoom";
 
 type RoomListProps = {
   rooms: Nullable<Array<any>>;
@@ -16,82 +14,7 @@ type RoomListProps = {
 const RoomList = (props: RoomListProps) => {
   const totalPages = Math.ceil((props.rooms ?? []).length / PAGE_MAX_ITEMS);
   const currentPage = usePageFromSearchParams(totalPages);
-  const [localRooms, setLocalRooms] = useState(props.rooms);
-
-  const [state, setState] = useState<
-    "default" | "additionAnimate" | "addition" | "deletionAnimate" | "deletion"
-  >("default");
-
-  const animateDeletion = () => {
-    const deletionRooms = localRooms?.map((localRoom) => {
-      if (!props.rooms?.find((room) => room._id === localRoom._id)) {
-        return { ...localRoom, animating: true, type: "deletion" };
-      }
-      return localRoom;
-    });
-    setLocalRooms(deletionRooms);
-
-    const timer = setTimeout(() => {
-      setState("deletion");
-    }, 500);
-    return () => clearTimeout(timer);
-  };
-
-  const deletion = () => {
-    setLocalRooms(localRooms?.filter((room) => room.type !== "deletion"));
-    setState("additionAnimate");
-  };
-
-  const animateAddition = () => {
-    let additionRooms = props.rooms?.map((room, index) => {
-      if (!localRooms?.find((localRoom) => localRoom._id === room._id)) {
-        return { ...room, animating: true, type: "addition" };
-      }
-      return room;
-    });
-    setLocalRooms(additionRooms);
-    const timer = setTimeout(() => {
-      setState("addition");
-    }, 500);
-    return () => clearTimeout(timer);
-  };
-
-  const addition = () => {
-    setLocalRooms(props.rooms);
-    setState("default");
-  };
-
-  useEffect(() => {
-    switch (state) {
-      case "deletionAnimate":
-        animateDeletion();
-        break;
-      case "deletion":
-        deletion();
-        break;
-      case "additionAnimate":
-        animateAddition();
-        break;
-      case "addition":
-        addition();
-        break;
-      default:
-        break;
-    }
-  }, [state]);
-
-  useEffect(() => {
-    if (
-      props.rooms &&
-      localRooms?.length &&
-      (props.rooms.length !== localRooms.length ||
-        JSON.stringify(props.rooms) !== JSON.stringify(localRooms.length))
-    ) {
-      setState("deletionAnimate");
-    } else {
-      setLocalRooms(props.rooms);
-    }
-  }, [props.rooms]);
+  const { localRooms } = useRoomListAnimationState(props.rooms);
 
   return (
     <>
@@ -109,11 +32,11 @@ const RoomList = (props: RoomListProps) => {
                 replace
                 style={{ textDecoration: "none" }}
               >
-                {room.animating ? (
-                  <NewRoom room={room} marginBottom="15px" type={room.type} />
-                ) : (
-                  <Room data={room} marginBottom="15px" />
-                )}
+                <AnimatedRoom
+                  data={room}
+                  marginBottom="15px"
+                  type={room.type}
+                />
               </Link>
             ))}
           {localRooms.length > PAGE_MAX_ITEMS && (
