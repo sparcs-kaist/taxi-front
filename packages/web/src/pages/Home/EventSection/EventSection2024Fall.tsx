24aeaf7cb875fc2863246ab24ff94ef6ce58fccd
static <kmc7468@naver.com>
2024-09-05 21:08:30 +0900

    Refactor: home page event section text

--- packages/web/src/pages/Home/EventSection/EventSection2024Fall.tsx
+++ packages/web/src/pages/Home/EventSection/EventSection2024Fall.tsx
@@ -1,15 +1,14 @@
-import { ReactElement, useCallback, useEffect, useState } from "react";
+import { ReactElement } from "react";
 import { Link } from "react-router-dom";
 
 import AdaptiveDiv from "@/components/AdaptiveDiv";
-import CreditAmountStatusContainer from "@/components/Event/CreditAmountStatusContainer";
-import WhiteContainerSuggestJoinEvent from "@/components/Event/WhiteContainerSuggestJoinEvent";
+// import CreditAmountStatusContainer from "@/components/Event/CreditAmountStatusContainer";
+// import WhiteContainerSuggestJoinEvent from "@/components/Event/WhiteContainerSuggestJoinEvent";
 import Title from "@/components/Title";
 import WhiteContainer from "@/components/WhiteContainer";
 
 import theme from "@/tools/theme";
 
-// ToDo : 2023fall 이미지
 import { ReactComponent as BackgroundMain } from "@/static/events/2023fallHomeMain.svg";
 import { ReactComponent as BackgroundMission } from "@/static/events/2023fallHomeMission.svg";
 import { ReactComponent as BackgroundStore } from "@/static/events/2023fallHomeStore.svg";
@@ -67,35 +66,25 @@ const ButtonContainer = ({
 };
 
 const EventSection2024Fall = () => {
-  const [amountType, setAmountType] = useState<"credit" | "ticket">("credit");
-  const changeAmountType = useCallback(
-    () => setAmountType((prev) => (prev === "credit" ? "ticket" : "credit")),
-    []
-  );
-  useEffect(() => {
-    const interval = setInterval(changeAmountType, 3000);
-    return () => clearInterval(interval);
-  });
-
   return (
     <AdaptiveDiv type="center">
       <Title icon="festival" isHeader>
         추석 이벤트
       </Title>
-      <CreditAmountStatusContainer
+      {/* <CreditAmountStatusContainer
         type={amountType}
         css={{ ...theme.cursor() }}
         onClick={changeAmountType}
-      />
+      /> */}
       <div css={{ display: "flex", gap: "15px" }}>
         <Link to="/event/2024fall" css={{ width: 0, flexGrow: 1 }}>
           <ButtonContainer
             title="이벤트 안내"
             description={
               <>
-                2024/09/09 - 09/23
+                2024/09/07 - 09/23
                 <br />
-                절찬리 진행 중!
+                진행 예정!
               </>
             }
           >
@@ -115,12 +104,12 @@ const EventSection2024Fall = () => {
         <div css={{ width: 0, flexGrow: 1, opacity: 0.5 }}>
           {/* <Link to="/event/2024fall-store" > */}
           <ButtonContainer
-            title="뽑기장"
+            title="응모권 교환소"
             description={
               <>
-                응모권을 사서
+                응모권 구매해서
                 <br />
-                상품 추첨에 참여하기
+                경품 추첨에 참여하기
               </>
             }
           >
@@ -129,7 +118,7 @@ const EventSection2024Fall = () => {
           {/* </Link> */}
         </div>
       </div>
-      <WhiteContainerSuggestJoinEvent />
+      {/* <WhiteContainerSuggestJoinEvent /> */}
     </AdaptiveDiv>
   );
 };
