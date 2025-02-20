4de632eb2192a24df277eb499035695fe98cea0a
static <kmc7468@naver.com>
2025-02-21 00:33:56 +0900

    Remove: onClose prop of ModalElem

--- packages/web/src/components/Modal/ModalElem.tsx
+++ packages/web/src/components/Modal/ModalElem.tsx
@@ -29,7 +29,6 @@ export type ModalElemProps = {
   backgroundChildren?: ReactNode;
   children?: ReactNode;
   isAlert?: boolean;
-  onClose?: () => void;
 };
 
 const ModalElem = ({
@@ -43,17 +42,16 @@ const ModalElem = ({
   backgroundChildren,
   children,
   isAlert = false,
-  onClose,
 }: ModalElemProps) => {
   const [display, setDisplay] = useState(false);
   const shouldMount = useDelayBoolean(isOpen, theme.duration_num);
   const modalRef = useRef<HTMLDivElement>(null);
   const clickRef = useRef(false);
 
-  const closeHandler = useCallback(() => {
-    onClose?.();
-    onChangeIsOpen?.(false);
-  }, [onClose, onChangeIsOpen]);
+  const closeHandler = useCallback(
+    onChangeIsOpen ? () => onChangeIsOpen(false) : () => {},
+    [onChangeIsOpen]
+  );
   const onMouseDown = useCallback(({ target }: MouseEvent) => {
     if (!modalRef.current?.contains(target as Node)) {
       clickRef.current = true;
