13761beebbded416b37f7d910a233172f5c96896
static <kmc7468@naver.com>
2025-02-21 01:10:37 +0900

    Refactor: update text

--- packages/web/src/pages/Event/Event2025SpringStore/Leaderboard.tsx
+++ packages/web/src/pages/Event/Event2025SpringStore/Leaderboard.tsx
@@ -279,7 +279,7 @@ const Event2025SpringStoreLeaderboard = ({
         : event2025SpringInfo.isAgreeOnTermsOfEvent === false
         ? [true, "이벤트에 참여해야 합니다"]
         : event2025SpringInfo.creditAmount < (item?.price * purchaseAmount || 0)
-        ? [true, "송편코인이 부족합니다"]
+        ? [true, "넙죽코인이 부족합니다"]
         : [false, "응모권 구매하기"],
     [eventMode, event2025SpringInfo, item, purchaseAmount]
   );
