3e5a26d24c28d8946530bdc1da4f94721a657b1b
jinhyeonkwon <athexplorer@kaist.ac.kr>
2025-04-29 22:42:45 +0900

    feat: calling createFavorite

--- packages/web/src/components/FavoriteRoutes/index.tsx
+++ packages/web/src/components/FavoriteRoutes/index.tsx
@@ -1,4 +1,8 @@
+import { useCallback, useRef } from "react";
+
 import { useValueRecoilState } from "@/hooks/useFetchRecoilState";
+import { useFetchFavoriteRoutes } from "@/hooks/useFetchRecoilState/useFetchFavoriteRoutes";
+import { useAxios } from "@/hooks/useTaxiAPI";
 
 import { FavoriteRouteType } from "@/atoms/favoriteRoutes";
 
@@ -13,10 +17,43 @@ const FavoriteRoutes = () => {
     columnGap: "8px",
   };
   const favoriteRoutes = useValueRecoilState("favoriteRoutes") || [];
+  const isAxiosCalled = useRef(false);
+  const axios = useAxios();
+  const fetchFavoriteRoutes = useFetchFavoriteRoutes();
+
+  const onCreateFavorite = useCallback(
+    async (from: string, to: string) => {
+      if (isAxiosCalled.current) return;
+      isAxiosCalled.current = true;
+      const data = {
+        from: from,
+        to: to,
+      };
+      await axios({
+        url: "/users/createFavorite",
+        method: "post",
+        data,
+      }).then(() => {
+        fetchFavoriteRoutes();
+        isAxiosCalled.current = false;
+      });
+    },
+    [fetchFavoriteRoutes, isAxiosCalled]
+  );
 
   return (
     <div>
-      <div style={styleTop}>으하하</div>
+      <div
+        style={styleTop}
+        onClick={() => {
+          onCreateFavorite(
+            "67985fcebe99bc902387d0c3",
+            "67985fcebe99bc902387d0c9"
+          );
+        }}
+      >
+        으하하
+      </div>
       {favoriteRoutes.data.map((route: FavoriteRouteType) => (
         <div key={route.from._id}>
           {route.from.koName} - {route.to.koName}
