bd1b09eb3f26b670ac89374902d5eb97694373af
malloc3141 <malloc_3141592@naver.com>
2026-04-02 15:08:59 +0900

    Add: re-add settlment feature

--- packages/web/src/components/Chat/MessagesBody/MessageSet/MessageAccount.tsx
+++ packages/web/src/components/Chat/MessagesBody/MessageSet/MessageAccount.tsx
@@ -1,8 +1,9 @@
 import { useMemo, useState } from "react";
 
+import useSettlementFromChats from "@/hooks/chat/useSettlementFromChats";
 import { useValueRecoilState } from "@/hooks/useFetchRecoilState";
 
-import { ModalChatPayement } from "@/components/ModalPopup";
+import { ModalChatPayment } from "@/components/ModalPopup";
 
 import Button from "./Button";
 
@@ -13,15 +14,23 @@ import WalletRoundedIcon from "@mui/icons-material/WalletRounded";
 type MessageAccountProps = {
   roomInfo: Room;
   account: string;
+  settlement: ReturnType<typeof useSettlementFromChats>;
 };
 
-const MessageAccount = ({ roomInfo, account }: MessageAccountProps) => {
+const MessageAccount = ({
+  roomInfo,
+  account,
+  settlement,
+}: MessageAccountProps) => {
   const { oid: userOid } = useValueRecoilState("loginInfo") || {};
   const [isOpenPayment, setIsOpenPayment] = useState<boolean>(false);
   const [bankName, accountNumber] = useMemo((): [string, string] => {
     const splited = account.split(" ");
     return [splited?.[0] || "", splited?.[1] || ""];
   }, [account]);
+  const [totalAmount, perPersonAmount] = useMemo((): [number, number] => {
+    return [settlement?.total || 0, settlement?.perPerson || 0];
+  }, [settlement]);
   const settlementStatusForMe = useMemo(
     () =>
       roomInfo &&
@@ -41,11 +50,16 @@ const MessageAccount = ({ roomInfo, account }: MessageAccountProps) => {
     height: "32px",
     backgroundColor: theme.purple,
   };
-  const styleBody = { padding: "10px" };
+  const styleBody = {
+    padding: "10px",
+    display: "flex",
+    flexDirection: "column" as const,
+    gap: "6px",
+  };
   const styleLine = { display: "flex", gap: "6px" };
   const styleLineLeft = { ...theme.font14, color: theme.gray_text };
   const styleLineRight = { ...theme.font14, color: theme.black };
-  const styleButtonSection = { display: "flex", gap: "6px", marginTop: "10px" };
+  const styleButtonSection = { display: "flex", gap: "6px", marginTop: "4px" };
 
   return (
     <div css={style}>
@@ -58,10 +72,17 @@ const MessageAccount = ({ roomInfo, account }: MessageAccountProps) => {
           <div css={styleLineLeft}>은행</div>
           <div css={styleLineRight}>{bankName}</div>
         </div>
-        <div css={{ height: "6px" }} />
         <div css={styleLine}>
           <div css={styleLineLeft}>계좌</div>
           <div css={styleLineRight}>{accountNumber}</div>
+        </div>
+        <div css={styleLine}>
+          <div css={styleLineLeft}>총 금액</div>
+          <div css={styleLineRight}>{totalAmount}원</div>
+        </div>
+        <div css={styleLine}>
+          <div css={styleLineLeft}>보낼 금액</div>
+          <div css={styleLineRight}>{perPersonAmount}원</div>
         </div>
         <div css={styleButtonSection}>
           <div css={{ flex: 1 }}>
@@ -80,11 +101,12 @@ const MessageAccount = ({ roomInfo, account }: MessageAccountProps) => {
           </div>
         </div>
       </div>
-      <ModalChatPayement
+      <ModalChatPayment
         isOpen={isOpenPayment}
         onChangeIsOpen={setIsOpenPayment}
         roomInfo={roomInfo}
         account={account}
+        settlement={settlement}
       />
     </div>
   );
